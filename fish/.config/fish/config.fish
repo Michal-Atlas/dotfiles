8f530afbefe57a0c03fea46a6600d9d6a592f87f
Michal Atlas <michal.z.atlas@gmail.com>
2021-06-08 08:19:33 +0200

    Changed fish themes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,11 @@
-set -gx TERM "xterm-kitty"
+#set -gx TERM "xterm-kitty"
 
 set PATH "$PATH:$HOME/.cargo/bin"
 set PATH "$PATH:$HOME/bin"
 set PATH "$PATH:$HOME/.local/bin"
 set PATH "$PATH:$HOME/.dotnet/bin"
 set PATH "$PATH:$HOME/Sync/bin"
+set PATH "$PATH:$HOME/source/scripts"
 set -gx EDITOR "/usr/bin/emacsclient"
 set -gx XDG_RUNTIME_DIR /run/user/1000
 set -gx RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/library 
@@ -13,14 +14,15 @@ function fish_greeting
 end
 
 abbr -a cr 'cargo run'
+abbr -a crd 'cargo doc --open'
 abbr -a rd 'xdg-open (rustup doc --path)'
 
-bind IDDQD 'sudo su'
-bind IDDT 'neofetch'
-bind IDMUS 'mpv --no-video "https://www.youtube.com/watch?v=Jly9qp40rfw"'
+#bind IDDQD 'sudo su'
+#bind IDDT 'neofetch'
+#bind IDMUS 'mpv --no-video "https://www.youtube.com/watch?v=Jly9qp40rfw"'
 
 starship init fish | source
-navi widget fish | source
+zoxide init --cmd j fish | source
 
 alias q exit
 alias vim nvim
