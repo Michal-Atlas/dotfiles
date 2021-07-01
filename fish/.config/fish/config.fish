ae6efeee85c4a4c92fa8439587a1734420b3a059
Michal Atlas <michal.z.atlas@gmail.com>
2021-07-01 18:58:14 +0200

    Added DOOM Emacs, NPM and Paru

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,13 @@
 #set -gx TERM "xterm-kitty"
 
 set PATH "$PATH:$HOME/.cargo/bin"
+set PATH "$PATH:$HOME/.npm-global/bin"
 set PATH "$PATH:$HOME/bin"
 set PATH "$PATH:$HOME/.local/bin"
 set PATH "$PATH:$HOME/.dotnet/bin"
 set PATH "$PATH:$HOME/Sync/bin"
 set PATH "$PATH:$HOME/source/scripts"
+set PATH "$PATH:$HOME/.emacs.d/bin"
 set -gx EDITOR "/usr/bin/emacsclient"
 set -gx XDG_RUNTIME_DIR /run/user/1000
 set -gx RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/library 
@@ -31,4 +33,18 @@ alias rm rip
 alias l ls
 alias ls "exa -a"
 alias ec emacsclient
+alias ecn "ec -nc"
 alias crontab "crontab -i"
+alias paru "paru --sudoloop --skipreview --bottomup"
+
+function vterm_printf;
+    if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end
+        # tell tmux to pass the escape sequences through
+        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+    else if string match -q -- "screen*" "$TERM"
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$argv"
+    else
+        printf "\e]%s\e\\" "$argv"
+    end
+end
