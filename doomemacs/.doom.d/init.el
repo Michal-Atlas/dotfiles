ee6dd72af113fb77186ca803a0326547c3fc35b5
Michal Atlas <michal.z.atlas@gmail.com>
2021-09-09 22:21:19 +0200

    Purged Evil

--- doomemacs/.doom.d/init.el
+++ doomemacs/.doom.d/init.el
@@ -53,7 +53,7 @@
        zen               ; distraction-free coding or writing
 
        :editor
-       (evil +everywhere); come to the dark side, we have cookies
+       ;;(evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
        (format +onsave)  ; automated prettiness
@@ -172,13 +172,13 @@
        ;;zig               ; C, but simpler
 
        :email
-       (mu4e +gmail +org)
+       ;;(mu4e +gmail +org)
        ;;notmuch
        ;;(wanderlust +gmail)
 
        :app
-       calendar
-       emms
+       ;;calendar
+       ;;emms
        everywhere        ; *leave* Emacs!? You must be joking
        irc               ; how neckbeards socialize
        (rss +org)        ; emacs as an RSS reader
