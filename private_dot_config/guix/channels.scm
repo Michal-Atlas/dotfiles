4c21022d0f4e74e8e4f89b1ad8b90a3bbc48193a
Michal Žáček <zacekmi2@fit.cvut.cz>
2022-01-05 15:22:58 +0100

    Added Atlas channel

--- private_dot_config/guix/channels.scm
+++ private_dot_config/guix/channels.scm
@@ -16,4 +16,7 @@
           "c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
           (openpgp-fingerprint
            "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))
+       (channel
+	(name 'atlas)
+	(url "https://github.com/Michal-Atlas/guix-channel.git"))
        %default-channels)
