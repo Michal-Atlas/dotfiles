c90e98edbdb489b244fe6b6f3997856c24f859eb
Michal Žáček <zacekmi2@fit.cvut.cz>
2022-01-07 00:37:49 +0100

    Added Hydra mounts

--- atlas/home/hydra.scm
+++ atlas/home/hydra.scm
@@ -25,6 +25,16 @@
 	     (uuid "bc1de881-5fec-4a9f-a2e7-4fc1db8e6404"))
 	    (type "btrfs")
 	    (options "subvol=@/home"))
+	   (file-system
+	    (mount-point "/Games")
+	    (device
+	     (uuid "f8099f31-3ab1-41d1-95c3-430bda2db004"))
+	    (type "ext4"))
+	   (file-system
+	    (mount-point "/Data")
+	    (device
+	     (uuid "E84A3BE94A3BB2E4" 'ntfs))
+	    (type "ntfs"))
 	   %base-file-systems))))
 
 hydra-system
