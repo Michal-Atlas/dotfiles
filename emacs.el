757f281e46e51c4b1540c0563cf67771e88bb97e
Michal Žáček <zacekmi2@fit.cvut.cz>
2022-01-31 20:03:08 +0100

    Removed requires from emacs.el

--- emacs.el
+++ emacs.el
@@ -48,7 +48,6 @@
 (show-paren-mode 1)
 (column-number-mode 1)
 
-(require 'highlight-indent-guides)
 (add-hook 'prog-mode-hook #'highlight-indent-guides-mode)
 (add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
 (setq highlight-indent-guides-method 'bitmap)
@@ -56,123 +55,78 @@
 (set-frame-font "Jetbrains Mono-8" nil t)
 (add-to-list 'default-frame-alist '(font . "Jetbrains Mono-8"))
 
-(require 'spacemacs-dark-theme)
 (load-theme 'spacemacs-dark t)
-
-(require 'doom-modeline)
 (doom-modeline-mode 1)
-
-(require 'solaire-mode)
 (solaire-global-mode +1)
-
-(require 'which-key)
 (which-key-mode)
 (setq which-key-popup-type 'minibuffer)
 
 (global-display-line-numbers-mode)
 (global-hl-line-mode 1)
 
-(require 'rainbow-identifiers)
 (add-hook 'prog-mode-hook #'rainbow-identifiers-mode)
-(require 'rainbow-delimiters)
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 
-;; Theming:1 ends here
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 
 ;; Formatting
 
-(require 'aggressive-indent)
 (global-aggressive-indent-mode 1)
 
-;; Formatting:1 ends here
-
-(require' crux)
-
-(require 'undo-tree)
 (global-undo-tree-mode 1)
 (setq undo-tree-auto-save-history t)
 (setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
 
 ;; Company
 
-;; (require 'company)
+;; 
 ;; (company-mode)
 ;; (add-hook 'after-init-hook #'global-company-mode)
 
-;; (require 'company-box)
+;; 
 ;; (add-hook 'company-mode-hook #'company-box-mode)
 ;; (setq company-box-icons-alist 'company-box-icons-all-the-icons)
 
-(require 'flycheck)
 (global-flycheck-mode)
 
-;; Company:1 ends here
-
-(require 'yasnippet)
-(yas-global-mode 1)
-(add-to-list 'yas-snippet-dirs "~/source/guix/etc/snippets")
-
-(require 'projectile)
 (projectile-mode +1)
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 ;; Vertico
 
-;; [[file:../dotfiles/conf.org::*Vertico][Vertico:1]]
-;; Enable vertico
-(require 'vertico)
 (vertico-mode)
 (setq vertico-count 15)
 
-
-(require 'orderless)
 (setq completion-styles '(orderless)
       completion-category-defaults nil
       completion-category-overrides '((file (styles partial-completion))))
 
 (setq enable-recursive-minibuffers t)
 
-;; Vertico:1 ends here
-
 ;; LSP
 
 (global-set-key (kbd "C-c c") 'compile)
-(require 'lsp-mode)
+
 (setq lsp-keymap-prefix "C-c l")
 (add-hook 'lsp-mode-hook       #'lsp-enable-which-key-integration)
 
-(require 'lsp-ui)
-
-;; LSP:1 ends here
-
 ;; Magit
 
-(require 'magit)
 (global-set-key (kbd "C-c v s") 'magit-stage)
 (global-set-key (kbd "C-c v p") 'magit-push)
 (global-set-key (kbd "C-c v f") 'magit-pull)
 (global-set-key (kbd "C-c v c") 'magit-commit)
 
-(require 'magit-todos)
-
-;; Magit:1 ends here
-
-(require 'guix)
-(require 'adaptive-wrap)
-(require 'calfw)
-(require 'calfw-org)
 (global-set-key (kbd "C-c o c") 'cfw:open-calendar-buffer)
-(require 'pdf-tools)
-(require 'all-the-icons-dired)
+
 (add-hook 'dired-mode-hook #'all-the-icons-dired-mode)
-(require 'git-gutter)
+
 (global-git-gutter-mode +1)
-(require 'org-fragtog)
+
 (add-hook 'org-mode-hook #'org-fragtog-mode)
-(require 'avy)
+
 (global-set-key (kbd "M-q") 'avy-goto-word-0)
-(require 'anzu)
+
 (global-anzu-mode +1)
 (global-set-key (kbd "M-%") 'anzu-query-replace)
 (global-set-key (kbd "C-M-%") 'anzu-query-replace-regexp)
@@ -185,25 +139,15 @@
    (dot . t)
    (C . t)
    (shell . t)
+   (scheme . t)
    ))
 
-(require 'org-present)
-(require 'org-superstar)
 (add-hook 'org-mode-hook #'org-superstar-mode)
 
-;; Org Mode:1 ends here
-
-;; Marginalia
-
-;; Enable richer annotations using the Marginalia package
-(require 'marginalia)
 (marginalia-mode)
-
-;; Marginalia:1 ends here
 
 ;; Embark and Consult
 
-(require 'embark)
 (global-set-key (kbd "C-.") 'embark-act)
 (global-set-key (kbd "C-\"") 'embark-dwim)
 (global-set-key (kbd "C-h B") 'embark-bindings)
@@ -221,19 +165,14 @@
 
 ;; Guile
 
-(require 'ac-geiser)
 (ac-config-default)
 (add-hook 'geiser-mode-hook 'ac-geiser-setup)
 (add-hook 'geiser-repl-mode-hook 'ac-geiser-setup)
 (add-to-list 'ac-modes' geiser-repl-mode)
 
-(require 'paredit)
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (add-hook 'scheme-mode-hook #'enable-paredit-mode)
 
-(require 'iedit)
-
-(require 'multiple-cursors)
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
@@ -241,21 +180,18 @@
 
 ;; C
 
-(require' irony)
 (add-hook 'c-mode-hook #'irony-mode)
 (add-hook 'c-mode-hook #'lsp)
 (add-hook 'c++-mode-hook #'irony-mode)
 (add-hook 'c++-mode-hook #'lsp)
 (add-hook 'irony-mode-hook #'irony-cdb-autosetup-compile-options)
-(require 'irony-eldoc)
+
 (add-hook 'irony-mode-hook #'irony-eldoc)
-;; C:1 ends here
 
 (add-hook 'shell-script-mode 'prog-mode)
 
 ;; Elfeed
 
-(require 'elfeed)
 (setq elfeed-feeds
       '(("https://xkcd.com/rss.xml" comics)
 	("https://www.smbc-comics.com/comic/rss" comics)
@@ -280,10 +216,3 @@
    '("2b9dc43b786e36f68a9fd4b36dd050509a0e32fe3b0a803310661edb7402b8b6" "1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" default)))
 
 (provide 'init)
-;;; init.el ends here
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
