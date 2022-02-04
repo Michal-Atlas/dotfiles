f4a73e078c556160d131f8d3bde16454b046e7c8
Michal Žáček <zacekmi2@fit.cvut.cz>
2022-02-04 04:10:59 +0100

    Commited the smallest system for fooling around

---
+++ atlas/system/nanosys.scm
@@ -0,0 +1,39 @@
+(define-module (atlas system nanosys)
+  #:use-module (gnu)
+  #:use-module (gnu packages))
+
+(use-package-modules
+ text-editors
+ shells
+ emacs
+ linux)
+
+(define-public %nano-system
+  (operating-system
+   (host-name "Progtest")
+   (kernel linux-libre)
+   (locale "en_US.utf8")
+   (timezone "Europe/Prague")
+   (keyboard-layout
+    (keyboard-layout "us,cz" ",qwerty" #:options '("grp:caps_switch" "lv3:ralt_switch" "compose:rctrl-altgr")))
+   (users (cons* (user-account
+		  (name "progtest")
+		  (comment "Progtest")
+		  (group "users")
+		  (password (crypt "progtest" "salt"))
+		  (shell (file-append (specification->package "bash") "/bin/bash"))
+		  (supplementary-groups
+		   '("netdev" "audio" "video")))
+		 %base-user-accounts))
+   (packages
+    (cons
+     emacs
+     %base-packages))
+   (bootloader
+    (bootloader-configuration
+     (bootloader grub-efi-bootloader)
+     (targets `("/boot/efi"))))
+   (file-systems %base-file-systems)
+   (name-service-switch %mdns-host-lookup-nss)))
+
+%nano-system
