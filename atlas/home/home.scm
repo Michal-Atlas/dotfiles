a4e3cd51ce62cae209a549a4b5fc878e84ca098b
Michal Žáček <zacekmi2@fit.cvut.cz>
2022-02-21 23:31:17 +0100

    Removed home provenance, was broken

--- atlas/home/home.scm
+++ atlas/home/home.scm
@@ -19,7 +19,6 @@
        (hash-ref %packages-by-host (vector-ref (uname) 1))))
  (services
   (list
-   (service home-provenance-service-type)
    (service
     home-mcron-service-type
     (home-mcron-configuration
