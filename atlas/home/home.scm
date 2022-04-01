5887ef22fa39ea6f1461efcc02235e9d87914480
Michal Atlas <zacekmi2@fit.cvut.cz>
2022-04-01 05:22:07 +0200

    Guix home edits, env

--- atlas/home/home.scm
+++ atlas/home/home.scm
@@ -34,49 +34,29 @@
 	  '(next-minute '(0))
 	  "find $HOME/tmp -mindepth 1 -ctime +2 -empty -delete | xargs rm -r")
        ))))
-   (service
-    home-shepherd-service-type
-    (home-shepherd-configuration
-     (services
-      (list
-       (shepherd-service
-	(provision  '(emacs))
-	(documentation "Run `emacs' on startup")
-	(start #~(make-forkexec-constructor
-		  (list
-		   #$(file-append emacs-next "/bin/emacs")
-		   "--fg-daemon")
-		  #:user "michal-atlas"
-		  #:environment-variables
-		  (cons*
-		   "GDK_SCALE=2"
-		   "GDK_DPI_SCALE=0.41"
-		   (default-environment-variables))))
-	(stop #~(make-system-destructor
-		 "emacsclient -e '(save-buffers-kill-emacs)'"))
-	(respawn? #t)))
-      )))
-   (service home-bash-service-type
+    (service home-bash-service-type
 	    (home-bash-configuration
 	     (guix-defaults? #t)))
    (simple-service
     'dotfiles
     home-files-service-type
-    (list `("ssh/config" ,(local-file "../../ssh"))
-	  `("emacs.d/init.el" ,(local-file "../../emacs.el"))
-	  `("guile" ,(local-file "../../guile"))
-	  `("screenrc" ,(local-file "../../screen"))
-	  `("config/kitty/kitty.conf" ,(local-file "../../kitty.conf"))
-	  `("config/guix/channels.scm" ,(local-file "../../channels.scm"))))
+    `(list ("ssh/config" ,(local-file "../../ssh"))
+	   ("emacs.d/init.el" ,(local-file "../../emacs.el"))
+	   ("guile" ,(local-file "../../guile"))
+	   ("screenrc" ,(local-file "../../screen"))
+	   ("config/kitty/kitty.conf" ,(local-file "../../kitty.conf"))
+	   ("config/guix/channels.scm" ,(local-file "../../channels.scm"))
+	   ("mbsyncrc" ,(local-file "../../.mbsyncrc"))))
    (service
     home-zsh-service-type
     (home-zsh-configuration
      (environment-variables
       `(("BROWSER" . "firefox")
 	("SHELL" . "zsh")
-	("EDITOR" . "\"emacsclient -c\"")
+	("EDITOR" . "\"emacsclient -a=\"\"\"")
 	("XCURSOR_THEME" . "Adwaita")
-        ("XCURSOR_SIZE" . "36")))
+        ("XCURSOR_SIZE" . "36")
+	("TERM" . "xterm-256color")))
      (zshenv
       (list (local-file "../../zsh/env")))
      (zshrc
