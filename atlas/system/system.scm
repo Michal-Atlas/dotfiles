50df701c53136d2b6feff228ccc7a1a60cd1e175
Michal Atlas <zacekmi2@fit.cvut.cz>
2022-04-20 18:58:01 +0200

    Changed user shell to bash

--- atlas/system/system.scm
+++ atlas/system/system.scm
@@ -34,7 +34,7 @@
 		  (comment "Michal Atlas")
 		  (group "users")
 		  (home-directory "/home/michal_atlas")
-		  (shell (file-append (specification->package "zsh") "/bin/zsh"))
+		  (shell (file-append (specification->package "bash") "/bin/bash"))
 		  (supplementary-groups
 		   '("wheel" "netdev" "audio"
 		     "video" "libvirt" "kvm")))
