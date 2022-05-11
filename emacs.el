46b99dc08b64362729281641486b4d28779933eb
Michal Atlas <michal.z.atlas@gmail.com>
2022-05-11 21:31:58 +0200

    Made Emacs setup work independently

--- emacs.el
+++ emacs.el
@@ -3,6 +3,37 @@
 ;;; Code:
 ;; Variable Init
 
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(package-install 'use-package)
+
+(eval-when-compile
+  (require 'use-package)
+
+  (unless (package-installed-p 'quelpa)
+    (with-temp-buffer
+      (url-insert-file-contents "https://raw.githubusercontent.com/quelpa/quelpa/master/quelpa.el")
+      (eval-buffer)
+      (quelpa-self-upgrade)))
+  (require 'quelpa)
+  (quelpa
+   '(quelpa-use-package
+     :fetcher git
+     :url "https://github.com/quelpa/quelpa-use-package.git"))
+  (require 'quelpa-use-package))
+(require 'bind-key)
+
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+(use-package auto-package-update
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
+
 ;; (setq user-full-name "Michal Atlas"
 ;;       user-mail-address "michal.z.atlas@gmail.com")
 (setq user-full-name "Michal Žáček"
@@ -32,29 +63,46 @@
 (show-paren-mode 1)
 (column-number-mode 1)
 
-(add-hook 'prog-mode-hook #'highlight-indent-guides-mode)
-(setq highlight-indent-guides-method 'bitmap)
+(use-package highlight-indentation
+ :hook (prog-mode . highlight-indentation-mode)
+ :custom (highlight-indent-guides-method 'bitmap))
 
-(set-frame-font "Fira Code-11" nil t)
-(add-to-list 'default-frame-alist '(font . "Fira Code-11"))
+; (set-frame-font "Fira Code-11" nil t)
+; (add-to-list 'default-frame-alist '(font . "Fira Code-11"))
 
-(load-theme 'zerodark t)
-(zerodark-setup-modeline-format)
-(solaire-global-mode +1)
-(which-key-mode)
+(use-package flycheck)
+
+;; Magit
+
+(use-package magit
+  :bind (("C-c v s" . magit-stage)
+	 ("C-c v p" . magit-push)
+	 ("C-c v f" . magit-pull)
+	 ("C-c v c" . magit-commit)))
+
+(use-package zerodark-theme
+  :config (load-theme 'zerodark t)
+  (zerodark-setup-modeline-format))
+(use-package solaire-mode
+  :config (solaire-global-mode +1))
+(use-package which-key
+  :config (which-key-mode))
 (setq which-key-popup-type 'minibuffer)
 
 (global-display-line-numbers-mode)
 (global-hl-line-mode 1)
 
-(add-hook 'prog-mode-hook #'rainbow-identifiers-mode)
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(use-package rainbow-identifiers
+  :hook (prog-mode . rainbow-identifiers-mode))
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 (set-default 'preview-scale-function 1.5)
 
-(global-undo-tree-mode 1)
+(use-package undo-tree
+  :config (global-undo-tree-mode 1)
 (setq undo-tree-auto-save-history t)
-(setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
+(setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo"))))
 
 ;; (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
 ;; (setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling 
@@ -63,7 +111,9 @@
 ;; (setq auto-window-vscroll nil)
 ;; (setq scroll-conservatively 10000)
 
-(guix-prettify-global-mode +1)
+;; (guix-prettify-global-mode +1)
+
+(use-package aggressive-indent)
 
 ;; Eshell
 
@@ -82,55 +132,98 @@
 	    (require 'eshell-z)))
 
 (require 'eshell)
-(eshell-syntax-highlighting-global-mode 1)
+(use-package eshell-syntax-highlighting
+  :config (eshell-syntax-highlighting-global-mode 1))
 (setq eshell-review-quick-commands nil)
 (require 'esh-module) ; require modules
 (add-to-list 'eshell-modules-list 'eshell-tramp)
-(require 'equake)
-(advice-add #'save-buffers-kill-terminal :before-while #'equake-kill-emacs-advice)
-(require 'esh-autosuggest)
-(add-hook 'eshell-mode-hook #'esh-autosuggest-mode)
-
-;; Vertico
-
-(vertico-mode)
-(setq vertico-count 15)
-
-(setq completion-styles '(orderless)
-      completion-category-defaults nil
-      completion-category-overrides '((file (styles partial-completion))))
-
-(setq enable-recursive-minibuffers t)
+;;(require 'equake)
+;;(advice-add #'save-buffers-kill-terminal :before-while #'equake-kill-emacs-advice)
+(use-package esh-autosuggest
+  :hook (eshell-mode . esh-autosuggest-mode))
 
 ;; LSP
 
-(global-set-key (kbd "C-c c") 'compile)
-
-(setq lsp-keymap-prefix "C-c l")
-(add-hook 'lsp-mode-hook       #'lsp-enable-which-key-integration)
-
-;; Magit
-
-(global-set-key (kbd "C-c v s") 'magit-stage)
-(global-set-key (kbd "C-c v p") 'magit-push)
-(global-set-key (kbd "C-c v f") 'magit-pull)
-(global-set-key (kbd "C-c v c") 'magit-commit)
+(use-package lsp-mode
+  :bind ("C-c c" . compile)
+  :custom (lsp-keymap-prefix "C-c l")
+  :hook (lsp-mode . lsp-enable-which-key-integration))
 
 (global-set-key (kbd "C-c o c") 'cfw:open-calendar-buffer)
 
 (add-hook 'dired-mode-hook #'all-the-icons-dired-mode)
 
-(global-git-gutter-mode +1)
+(use-package git-gutter
+  :config (global-git-gutter-mode +1))
 
 (add-hook 'org-mode-hook #'org-fragtog-mode)
 (add-hook 'org-mode-hook #'org-superstar-mode)
 
-(avy-setup-default)
-(global-set-key (kbd "C-:") 'avy-goto-char-timer)
+;; Vertico
 
-(global-anzu-mode +1)
-(global-set-key (kbd "M-%") 'anzu-query-replace)
-(global-set-key (kbd "C-M-%") 'anzu-query-replace-regexp)
+(use-package vertico
+  :init
+  (vertico-mode)
+
+  ;; Different scroll margin
+  (setq vertico-scroll-margin 0)
+
+  ;; Show more candidates
+  (setq vertico-count 20)
+
+  ;; Grow and shrink the Vertico minibuffer
+  (setq vertico-resize t)
+
+  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+  (setq vertico-cycle t)
+  )
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
+;; A few more useful configurations...
+(use-package emacs
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+                  (replace-regexp-in-string
+                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                   crm-separator)
+                  (car args))
+          (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
+  ;; Vertico commands are hidden in normal buffers.
+  ;; (setq read-extended-command-predicate
+  ;;       #'command-completion-default-include-p)
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t))
+
+;; Optionally use the `orderless' completion style.
+(use-package orderless
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+(use-package anzu
+  :config (global-anzu-mode +1)
+  :bind (("M-%" . anzu-query-replace)
+	 ("C-M-%" . anzu-query-replace-regexp)))
 
 ;; Org Mode
 
@@ -143,28 +236,15 @@
    (scheme . t)
    ))
 
-(marginalia-mode)
-
-;; Embark and Consult
-
-(global-set-key (kbd "C-.") 'embark-act)
-(global-set-key (kbd "C-\"") 'embark-dwim)
-(global-set-key (kbd "C-h B") 'embark-bindings)
-
-;; Optionally replace the key help with a completing-read interface
-(setq prefix-help-command #'embark-prefix-help-command)
-
-;; Hide the mode line of the Embark live/completions buffers
-(add-to-list 'display-buffer-alist
-	     '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
-	       nil
-	       (window-parameters (mode-line-format . none))))
+(use-package marginalia
+  :config (marginalia-mode))
 
 ;; Langs
 
 ;; Lisps
 
-(ac-config-default)
+(use-package auto-complete
+  :config (ac-config-default))
 (add-hook 'geiser-mode-hook 'ac-geiser-setup)
 (add-hook 'geiser-repl-mode-hook 'ac-geiser-setup)
 (add-to-list 'ac-modes' geiser-repl-mode)
@@ -174,16 +254,16 @@
 (add-hook 'common-lisp-mode-hook #'slime-mode)
 (add-hook 'scheme-mode-hook #'geiser-mode)
 
-(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
-(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
-(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-(add-hook 'lisp-mode-hook             #'aggressive-indent-mode)
-(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
-(add-hook 'common-lisp-mode-hook           #'enable-paredit-mode)
-
+(use-package paredit
+  :config (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+  :hook ((emacs-lisp-mode . enable-paredit-mode)
+(eval-expression-minibuffer-setup . enable-paredit-mode)
+(ielm-mode . enable-paredit-mode)
+(lisp-mode . enable-paredit-mode)
+(lisp-mode . aggressive-indent-mode)
+(lisp-interaction-mode . enable-paredit-mode)
+(scheme-mode . enable-paredit-mode)
+(common-lisp-mode . enable-paredit-mode)))
 (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
 
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
@@ -221,3 +301,59 @@
 	("https://guix.gnu.org/feeds/blog.atom")))
 
 (provide 'init)
+
+(use-package equake
+  :quelpa (equake :fetcher gitlab :repo "emacsomancer/equake")
+  ;; some examples of optional settings follow:
+  :custom
+  ;; set width a bit less than full-screen (prevent 'overflow' on multi-monitor):
+  (equake-size-width 0.99)
+  ;; set distinct face for Equake: white foreground with dark blue background, and different font:
+  :config
+  ;; prevent accidental frame closure:
+  (advice-add #'save-buffers-kill-terminal :before-while #'equake-kill-emacs-advice)
+  ;; binding to restore last Equake tab when viewing a non-Equake buffer
+  (global-set-key (kbd "C-M-^") #'equake-restore-last-etab)
+  ;; set default shell
+  (setq equake-default-shell 'eshell)
+  ;; set list of available shells
+  (setq equake-available-shells
+	'("shell"
+	  "vterm"
+	  "rash"
+	  "eshell")))
+
+
+
+(use-package tex
+  :ensure auctex)
+
+(use-package helpful
+  :bind (("C-h f" . helpful-function)
+	 ("C-h k" . helpful-key)))
+
+(use-package crux)
+(use-package lsp-ui)
+(use-package magit-todos)
+(use-package adaptive-wrap)
+(use-package pdf-tools)
+(use-package avy)
+(use-package org-present)
+(use-package embark)
+(use-package consult)
+(use-package elpher)
+(use-package multiple-cursors)
+(use-package on-screen)
+(use-package notmuch)
+(use-package tldr)
+(use-package direnv)
+(use-package kana
+  :quelpa
+  (kana :repo "chenyanming/kana" :fetcher github))
+(use-package circe)
+(use-package browse-kill-ring
+  :config (browse-kill-ring-default-keybinding))
+(use-package realgud)
+(use-package gdscript-mode)
+(use-package emms)
+
