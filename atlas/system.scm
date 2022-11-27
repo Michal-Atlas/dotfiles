ee8696ec818e62dfce438b4c7d36375d5eecd513
Michal Atlas <michal_atlas+git@posteo.net>
2022-11-27 16:47:56 +0100

    Separate machines are going to have to be in one file it seems

---
+++ atlas/system.scm
@@ -0,0 +1,146 @@
+(define-module (atlas system)
+  #:use-module (atlas system packages)
+  #:use-module (atlas system services)
+  #:use-module (gnu)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages shells)
+  #:use-module (gnu packages emacs)
+  #:use-module (gnu packages samba)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu system setuid)
+  #:use-module (gnu services desktop)
+  #:use-module (gnu services xorg)
+  #:use-module (gnu home)
+  #:use-module (gnu home services)
+  #:use-module (gnu home services mcron)
+  #:use-module (gnu home services shells)
+  #:use-module (gnu home services shepherd)
+  #:use-module (nongnu packages linux)
+  #:use-module (nongnu system linux-initrd)
+  #:use-module (gnu packages hurd)
+  #:use-module (ice-9 textual-ports)
+  #:use-module (guix packages))
+
+(define-public atlas-system-base
+  (operating-system
+   (host-name #f)
+   (file-systems #f)
+   (kernel linux)
+   (initrd microcode-initrd)
+   (firmware (list linux-firmware))
+   (locale "en_US.utf8")
+   (timezone "Europe/Prague")
+   (keyboard-layout
+    (keyboard-layout "us,cz" ",ucw" #:options
+		     '("grp:caps_switch" #;"ctrl:nocaps" "grp_led"
+		       "lv3:ralt_switch" "compose:rctrl-altgr")))
+   (users (cons* (user-account
+		  (name "michal_atlas")
+		  (comment "Michal Atlas")
+		  (group "users")
+		  (home-directory "/home/michal_atlas")
+		  (supplementary-groups
+		   '("wheel" "netdev" "audio" "docker"
+		     "video" "libvirt" "kvm")))
+		 %base-user-accounts))
+   (packages
+    (append %system-desktop-manifest
+	    %base-packages))
+   (setuid-programs
+    (append (list (setuid-program
+                   (program (file-append cifs-utils "/sbin/mount.cifs")))
+		  (setuid-program
+		   (program (file-append light "/bin/light"))))
+            %setuid-programs))
+   (services
+    %system-services-manifest)
+   (bootloader
+    (bootloader-configuration
+     (bootloader grub-efi-bootloader)
+     (targets `("/boot/efi"))))
+   (name-service-switch %mdns-host-lookup-nss)))
+
+(operating-system
+ (inherit atlas-system-base)
+ (host-name "dagon")
+ (mapped-devices
+  (list (mapped-device
+	 (source
+	  (uuid "24276371-36b5-4d74-8376-8fc0b32f86cf"))
+	 (target "cryptroot")
+	 (type luks-device-mapping))))
+ (file-systems (cons*
+	 	(file-system
+		 (mount-point "/boot/efi")
+		 (device (uuid "317C-0555" 'fat32))
+		 (type "vfat"))
+		(file-system
+		 (mount-point "/")
+		 (device "/dev/mapper/cryptroot")
+		 (type "btrfs")
+		 (dependencies mapped-devices))
+		;; (file-system
+		;;  (mount-point "/boot/efi")
+		;;  (type "vfat")
+		;;  (device (file-system-label "EFI")))
+		;; (file-system
+		;;  (mount-point "/")
+		;;  (type "ext4")
+		;;  (device (file-system-label "guix")))
+		;; CTU
+		;; (file-system
+		;;  (device "//drive.fit.cvut.cz/home/zacekmi2")
+		;;  (mount-point "/fit")
+		;;  (title 'fit-cifs)
+		;;  (options "sec=ntlmv2i,fsc,file_mode=0700,dir_mode=0700,uid=1000,user=zacekmi2")
+		;;  (type "cifs")
+		;;  (mount? #f)
+		;;  (create-mount-point? #t))
+		%base-file-systems))
+ (swap-devices
+  (list (swap-space
+	 (target (uuid "6d46a910-394b-45e0-8a41-1e9f716c3864"))))))
+
+(define dagon
+  (operating-system
+ (inherit atlas-system-base)
+ (host-name "dagon")
+ (mapped-devices
+  (list (mapped-device
+	 (source
+	  (uuid "24276371-36b5-4d74-8376-8fc0b32f86cf"))
+	 (target "cryptroot")
+	 (type luks-device-mapping))))
+ (file-systems (cons*
+	 	(file-system
+		 (mount-point "/boot/efi")
+		 (device (uuid "317C-0555" 'fat32))
+		 (type "vfat"))
+		(file-system
+		 (mount-point "/")
+		 (device "/dev/mapper/cryptroot")
+		 (type "btrfs")
+		 (dependencies mapped-devices))
+		;; (file-system
+		;;  (mount-point "/boot/efi")
+		;;  (type "vfat")
+		;;  (device (file-system-label "EFI")))
+		;; (file-system
+		;;  (mount-point "/")
+		;;  (type "ext4")
+		;;  (device (file-system-label "guix")))
+		;; CTU
+		;; (file-system
+		;;  (device "//drive.fit.cvut.cz/home/zacekmi2")
+		;;  (mount-point "/fit")
+		;;  (title 'fit-cifs)
+		;;  (options "sec=ntlmv2i,fsc,file_mode=0700,dir_mode=0700,uid=1000,user=zacekmi2")
+		;;  (type "cifs")
+		;;  (mount? #f)
+		;;  (create-mount-point? #t))
+		%base-file-systems))
+ (swap-devices
+  (list (swap-space
+	 (target (uuid "6d46a910-394b-45e0-8a41-1e9f716c3864")))))))
+
+dagon
