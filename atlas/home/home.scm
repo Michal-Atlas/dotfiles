7133af2fddc3e047c2c6ade86add1d1865047591
Michal Atlas <michal_atlas+git@posteo.net>
2022-11-28 01:11:36 +0100

    Add Games channel

--- atlas/home/home.scm
+++ atlas/home/home.scm
@@ -131,6 +131,15 @@
 	     (channel
 	      (name 'atlas)
 	      (url "https://git.sr.ht/~michal_atlas/guix-channel"))
+	     (channel
+              (name 'guix-gaming-games)
+              (url "https://gitlab.com/guix-gaming-channels/games.git")
+              ;; Enable signature verification:
+              (introduction
+               (make-channel-introduction
+		"c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
+		(openpgp-fingerprint
+		 "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))
 	     %default-channels))
    (service home-openssh-service-type
 	    (home-openssh-configuration
