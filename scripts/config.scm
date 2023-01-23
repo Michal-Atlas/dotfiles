77ae706263990fe986ec4dcd6d306f157efcaa0c
Michal Atlas <michal_atlas+git@posteo.net>
2023-01-23 23:46:51 +0100

    Script knows about home move

--- scripts/config.scm
+++ scripts/config.scm
@@ -7,5 +7,5 @@
 
 (define-public channel-lock-file (string-append home "/dotfiles/channels.lock"))
 (define-public dotfile-dir (string-append home "/dotfiles"))
-(define-public home-config (string-append home "/dotfiles/atlas/home/home.scm"))
+(define-public home-config (string-append home "/dotfiles/atlas/home.scm"))
 (define-public system-config (string-append home "/dotfiles/atlas/system.scm"))
