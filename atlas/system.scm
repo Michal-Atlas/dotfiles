9ec0766bdcafa3d0ed60ba77a8f5bd63e7657a5c
Michal Atlas <michal_atlas+git@posteo.net>
2023-01-27 15:50:23 +0100

    Added tty privileges

--- atlas/system.scm
+++ atlas/system.scm
@@ -41,7 +41,7 @@
 		  (home-directory "/home/michal_atlas")
 		  (supplementary-groups
 		   '("wheel" "netdev" "audio" "docker"
-		     "video" "libvirt" "kvm")))
+		     "video" "libvirt" "kvm" "tty")))
 		 %base-user-accounts))
    (packages
     (append %system-desktop-manifest
