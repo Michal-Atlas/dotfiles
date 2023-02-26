2d8d0d7f2c7eb8e2938e5755aafbdaf01a9d48da
Michal Atlas <michal_atlas+git@posteo.net>
2023-02-26 18:12:57 +0100

    Added some random files

---
+++ machines.scm
@@ -0,0 +1,6 @@
+(list (build-machine
+       (name "hydra.local")
+       (systems (list "x86_64-linux"))
+       (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK3yNbz2Zmk5D2mDBjXtYKTxtSfnyrf2BIMljEfLTrSy root@(none)")
+       (user "michal_atlas")
+       (private-key "/home/michal_atlas/.ssh/id_rsa")))
