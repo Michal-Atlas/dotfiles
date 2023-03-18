6912939bfa87ef8cc4d72bbec21d10618f1381c6
Michal Atlas <michal_atlas+git@posteo.net>
2023-03-18 21:39:54 +0100

    Added mystic for fun

--- home.nix
+++ home.nix
@@ -175,6 +175,24 @@ in
     package = myEm;
   };
   home.packages = with pkgs; [
+    (pkgs.stdenv.mkDerivation
+      (
+        let version = "0.3"; in {
+          buildInputs = [ autoconf automake readline texinfo ];
+          pname = "mystic";
+          version = version;
+          src = fetchFromSourcehut {
+            owner = "~michal_atlas";
+            repo = "mystic";
+            rev = "v${version}";
+            sha256 = "sha256-1492bbgYfgbiRd3ahUFlaFFHrk1Scx+oTIjQamQ+m5o=";
+          };
+          preConfigurePhases = [ "autogen" ];
+          autogen = ''
+            ./autogen.sh
+          '';
+        }
+      ))
     myEm
     # clang
     # mathematica
