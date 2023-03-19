39db5c991790a52311908cb052a60753ffc542b3
Michal Atlas <michal_atlas+git@posteo.net>
2023-03-19 14:30:45 +0100

    Commit autofmt rule

--- flake.nix
+++ flake.nix
@@ -75,6 +75,17 @@
             # statix.enable = true;
             # shellcheck.enable = true;
             # shfmt.enable = true;
+
+            elisp-autofmt = {
+              enable = true;
+              name = "Elisp Autofmt";
+              entry = "${(pkgs.writeShellScriptBin "autofmt.sh" ''
+                ${
+                  (pkgs.emacsPackagesFor pkgs.emacs).emacsWithPackages
+                  (epkgs: with epkgs; [ elisp-autofmt ])
+                }/bin/emacs "$1" --batch --eval "(require 'elisp-autofmt)" -f elisp-autofmt-buffer -f save-buffer --kill'')}/bin/autofmt.sh";
+              files = "\\.(el)$";
+            };
           };
         };
       };
