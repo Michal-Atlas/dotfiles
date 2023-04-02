59f85bb37335cde42c69cb8f31e59d4fd7a33b6e
Michal Atlas <michal_atlas+git@posteo.net>
2023-04-02 14:59:52 +0200

    Added Maxima support, removed org-modern

--- home/dotfiles/config.el
+++ home/dotfiles/config.el
@@ -76,10 +76,10 @@
 
 ;; (use-package org-fragtog
 ;;   :hook (org-mode org-fragtog-mode))
-(use-package! org-modern :hook (org-mode . org-modern-mode))
 (org-babel-do-load-languages
  'org-babel-load-languages
- '((C . t) (scheme . t) (dot . t) (lisp . t)))
+ '((C . t) (scheme . t) (dot . t) (lisp . t)
+   (maxima . t)))
 
 ;; Variable Init
 
