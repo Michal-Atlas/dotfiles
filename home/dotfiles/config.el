4d10171cd3c774014730b5dc3a08ec4aaaf9f2f6
Michal Atlas <michal_atlas+git@posteo.net>
2023-04-02 14:59:20 +0200

    Removed debug-on-error

--- home/dotfiles/config.el
+++ home/dotfiles/config.el
@@ -72,9 +72,6 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-
-(setq debug-on-error t)
-
 ;; Org-mode
 
 ;; (use-package org-fragtog
