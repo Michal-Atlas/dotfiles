7ef9e1c26946fff4c3aa0ad7ca959f0bb141d9f4
Michal Atlas <michal_atlas+git@posteo.net>
2023-04-30 06:24:07 +0200

    Updated keys

--- machines.scm
+++ machines.scm
@@ -1,6 +1,6 @@
 (list (build-machine
        (name "hydra.local")
        (systems (list "x86_64-linux"))
-       (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK3yNbz2Zmk5D2mDBjXtYKTxtSfnyrf2BIMljEfLTrSy root@(none)")
+       (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHg7gxhmd2SLBDb3U44L3hxNU8o4dbqZAOnlMTyvgXwp root@(none)")
        (user "michal_atlas")
        (private-key "/home/michal_atlas/.ssh/id_rsa")))
