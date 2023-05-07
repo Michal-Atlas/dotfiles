854a7f7911f7d653551abef0180e5151b78913a2
Michal Atlas <michal_atlas+git@posteo.net>
2023-05-08 00:11:10 +0200

    Unify into Org-mode tangling file

--- machines.scm
+++
@@ -1,6 +0,0 @@
-(list (build-machine
-       (name "hydra.local")
-       (systems (list "x86_64-linux"))
-       (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHg7gxhmd2SLBDb3U44L3hxNU8o4dbqZAOnlMTyvgXwp root@(none)")
-       (user "michal_atlas")
-       (private-key "/home/michal_atlas/.ssh/id_rsa")))
