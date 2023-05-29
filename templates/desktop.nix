4c2f4391b86d26cb735bfd733f2b094f5ff05458
Michal Atlas <michal_atlas+git@posteo.net>
2023-05-29 23:23:20 +0200

    Templating Syncthing

--- templates/desktop.nix
+++ templates/desktop.nix
@@ -6,6 +6,7 @@
     ../cachix.nix
     ./yggdrasil.nix
     ./atlasnet-hosts.nix
+    ./syncthing.nix
   ];
   nix.settings.trusted-users = [ "root" "@wheel" ];
 
@@ -84,61 +85,6 @@
   services.zerotierone = {
     enable = true;
     joinNetworks = [ "c7c8172af19f63d7" ];
-  };
-
-  services.syncthing = {
-    enable = true;
-    user = "michal_atlas";
-    dataDir =
-      "/home/michal_atlas/Documents"; # Default folder for new synced folders
-    configDir =
-      "/home/michal_atlas/.config/syncthing"; # Folder for Syncthing's settings and keys
-    overrideDevices =
-      true; # overrides any devices added or deleted through the WebUI
-    overrideFolders =
-      true; # overrides any folders added or deleted through the WebUI
-    relay.enable = false;
-    settings = {
-      devices = {
-        "nox" = {
-          id = "JBRYVQP-2GYSCCK-2M37T6I-KSETJHC-UY7ZUQ5-GW56FMG-LDRDFQC-YUH5EAY";
-        };
-        "hydra" = {
-          id = "4F77KY2-XKLI7OD-J5GX6RH-VLFWIZA-M45YSV2-C2PNRGE-4GCE5Y5-ZLSTHQP";
-        };
-        "dagon" = {
-          id = "UOVQXCK-LGQ7OA5-YQUBF67-QHTENZK-KEEGPET-PLZZQFZ-BPSZRCJ-LVEBTAD";
-        };
-      };
-      folders = with builtins;
-        (listToAttrs
-          (concatLists [
-            (map
-              (name:
-                {
-                  name = "${name}";
-                  value = {
-                    path = "/home/michal_atlas/${name}";
-                    devices = [ "nox" "hydra" "dagon" ];
-                    versioning.type = "staggered";
-                  };
-                })
-              [ "cl" "Documents" ])
-            (map
-              (name:
-                {
-                  name = "${name}";
-                  value = {
-                    path = "/home/michal_atlas/${name}";
-                    devices = [ "nox" "hydra" "dagon" ];
-                  };
-                })
-              [
-                "Sync"
-                "Zotero"
-              ])
-          ]));
-    };
   };
 
   # Enable sound with pipewire.
---
+++ templates/syncthing.nix
@@ -0,0 +1,58 @@
+{ ... }: {
+
+  services.syncthing = {
+    enable = true;
+    user = "michal_atlas";
+    dataDir =
+      "/home/michal_atlas/Documents"; # Default folder for new synced folders
+    configDir =
+      "/home/michal_atlas/.config/syncthing"; # Folder for Syncthing's settings and keys
+    overrideDevices =
+      true; # overrides any devices added or deleted through the WebUI
+    overrideFolders =
+      true; # overrides any folders added or deleted through the WebUI
+    relay.enable = false;
+    settings = {
+      devices = {
+        "nox" = {
+          id = "JBRYVQP-2GYSCCK-2M37T6I-KSETJHC-UY7ZUQ5-GW56FMG-LDRDFQC-YUH5EAY";
+        };
+        "hydra" = {
+          id = "4F77KY2-XKLI7OD-J5GX6RH-VLFWIZA-M45YSV2-C2PNRGE-4GCE5Y5-ZLSTHQP";
+        };
+        "dagon" = {
+          id = "UOVQXCK-LGQ7OA5-YQUBF67-QHTENZK-KEEGPET-PLZZQFZ-BPSZRCJ-LVEBTAD";
+        };
+      };
+      folders = with builtins;
+        (listToAttrs
+          (concatLists [
+            (map
+              (name:
+                {
+                  name = "${name}";
+                  value = {
+                    path = "/home/michal_atlas/${name}";
+                    devices = [ "nox" "hydra" "dagon" ];
+                    versioning.type = "staggered";
+                  };
+                })
+              [ "cl" "Documents" ])
+            (map
+              (name:
+                {
+                  name = "${name}";
+                  value = {
+                    path = "/home/michal_atlas/${name}";
+                    devices = [ "nox" "hydra" "dagon" ];
+                  };
+                })
+              [
+                "Sync"
+                "Zotero"
+              ])
+          ]));
+    };
+  };
+
+}
