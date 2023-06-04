3d6b6db886500f1f69e8a0270f837aa00f0a7199
Michal Atlas <michal_atlas+git@posteo.net>
2023-06-04 17:00:28 +0200

    //ing two attrsets sadly takes only the right value

--- templates/atlasnet-hosts.nix
+++ templates/atlasnet-hosts.nix
@@ -1,18 +1,17 @@
-_:
-{
-  networking.hosts = {
-    "200:aa4d:e2e3:5360:2ee8:7b99:984f:155f" = [ "hydra" ];
-    "202:8a79:d8dc:19dd:d045:e857:6637:f8af" = [ "dagon" ];
-    "203:8b30:62b9:24a4:4af3:20cf:b37b:c4fe" = [ "arc" ];
-  } //
-  (builtins.foldl' (acc: addr: acc // { "0.0.0.0" = [ addr ]; "::0" = [ addr ]; })
-    { }
-    [
-      "reddit.com"
-      "www.youtube.com"
-      "m.youtube.com"
-      "youtu.be"
-    ]);
+_: {
+  networking.hosts =
+    let
+      blocklinks = [ "reddit.com" "www.youtube.com" "m.youtube.com" "youtu.be" ];
+    in
+    {
+      "200:aa4d:e2e3:5360:2ee8:7b99:984f:155f" = [ "hydra" ];
+      "202:8a79:d8dc:19dd:d045:e857:6637:f8af" = [ "dagon" ];
+      "203:8b30:62b9:24a4:4af3:20cf:b37b:c4fe" = [ "arc" ];
+    } //
+    {
+      "0.0.0.0" = blocklinks;
+      "::0" = blocklinks;
+    };
 
   networking.stevenBlackHosts = {
     enable = true;
