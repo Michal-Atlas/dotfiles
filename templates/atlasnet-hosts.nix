3ff5b6bf2500dc8da4aaa446d5656191ab00829c
Michal Atlas <michal_atlas+git@posteo.net>
2023-06-14 17:10:55 +0200

    Only blocking nasty sites not my own

--- templates/atlasnet-hosts.nix
+++ templates/atlasnet-hosts.nix
@@ -1,25 +1,9 @@
 _: {
-  networking.hosts =
-    let
-      blocklinks = [
-        "reddit.com"
-        "www.reddit.com"
-        "www.youtube.com"
-        "m.youtube.com"
-        "youtu.be"
-        "www.deviantart.com"
-        "www.tiktok.com"
-      ];
-    in
-    {
-      "200:aa4d:e2e3:5360:2ee8:7b99:984f:155f" = [ "hydra" ];
-      "202:8a79:d8dc:19dd:d045:e857:6637:f8af" = [ "dagon" ];
-      "203:8b30:62b9:24a4:4af3:20cf:b37b:c4fe" = [ "arc" ];
-    } //
-    {
-      "0.0.0.0" = blocklinks;
-      "::0" = blocklinks;
-    };
+  networking.hosts = {
+    "200:aa4d:e2e3:5360:2ee8:7b99:984f:155f" = [ "hydra" ];
+    "202:8a79:d8dc:19dd:d045:e857:6637:f8af" = [ "dagon" ];
+    "203:8b30:62b9:24a4:4af3:20cf:b37b:c4fe" = [ "arc" ];
+  };
 
   networking.stevenBlackHosts = {
     enable = true;
