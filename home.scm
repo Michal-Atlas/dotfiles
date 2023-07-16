b24d574892200735dab26af68abc0fb51433ac62
Michal Atlas <michal_atlas+git@posteo.net>
2023-07-16 03:51:51 +0200

    Separating out files

--- home.scm
+++ home.scm
@@ -169,7 +169,8 @@
                 (shepherd-service
 		        (provision '(emacs))
 		        (start #~(make-forkexec-constructor
-			              (list #$(file-append emacs "/bin/emacs") "--fg-daemon")
+			              (list #$(file-append emacs-next-pgtk "/bin/emacs")
+                                            "--fg-daemon")
 			              #:environment-variables
 			              (cons*
 			               "GDK_SCALE=2"
@@ -211,13 +212,12 @@
    (simple-service
     'dotfiles
     home-files-service-type
-    (map
-     (match-lambda [(out in) `(,out ,(local-file in))])
-     '((".emacs.d/init.el" "files/emacs.el")
-       (".guile" "files/guile.scm")
-       (".sbclrc" "files/sbcl.lisp")
-       (".local/share/nyxt/bookmarks.lisp" "files/nyxt/bookmarks.lisp")
-       (".config/nyxt/config.lisp" "files/nyxt/init.lisp"))))
+    ;; local-file being explicit allows earlier check for file existencex
+    `((".emacs.d/init.el" ,(local-file "files/emacs.el"))
+      (".guile" ,(local-file "files/guile.scm"))
+      (".sbclrc" ,(local-file "files/sbcl.lisp"))
+      (".local/share/nyxt/bookmarks.lisp" ,(local-file "files/nyxt/bookmarks.lisp"))
+      (".config/nyxt/config.lisp" ,(local-file "files/nyxt/init.lisp"))))
    
    (service
     home-bash-service-type
@@ -291,47 +291,7 @@
 ;; Environment:2 ends here
 
 ;; [[file:Dotfiles.org::*Environment][Environment:3]]
-(service home-channels-service-type
-	 (cons*
-	  (channel
-	   (name 'nonguix)
-	   (url "https://gitlab.com/nonguix/nonguix")
-	   (branch "master")
-	   (introduction
-	    (make-channel-introduction
-	     "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-	     (openpgp-fingerprint
-	      "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-	  (channel
-	   (name 'guixrus)
-	   (url "https://git.sr.ht/~whereiseveryone/guixrus")
-	   (introduction
-	    (make-channel-introduction
-	     "7c67c3a9f299517bfc4ce8235628657898dd26b2"
-	     (openpgp-fingerprint
-	      "CD2D 5EAA A98C CB37 DA91  D6B0 5F58 1664 7F8B E551"))))
-	  (channel
-	   (name 'atlas)
-	   (url "https://git.sr.ht/~michal_atlas/guix-channel"))
-	  (channel
-	   (name 'guix-gaming-games)
-	   (url "https://gitlab.com/guix-gaming-channels/games.git")
-	   (introduction
-	    (make-channel-introduction
-	     "c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
-	     (openpgp-fingerprint
-	      "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))
-	 		 #;
-	 		 (channel                   ; ; ; ; ; ; ; ; ; ; ; ; ; ; ;
-	 		 (name 'emacs)              ; ; ; ; ; ; ; ; ; ; ; ; ; ; ;
-	 		 (url "https://github.com/babariviere/guix-emacs") ; ; ; ; ; ; ; ; ; ; ; ; ; ; ;
-	 		 (introduction              ; ; ; ; ; ; ; ; ; ; ; ; ; ; ;
-	 		 (make-channel-introduction ; ; ; ; ; ; ; ; ; ; ; ; ; ; ;
-	 		 "72ca4ef5b572fea10a4589c37264fa35d4564783" ; ; ; ; ; ; ; ; ; ; ; ; ; ; ;
-	 		 (openpgp-fingerprint       ; ; ; ; ; ; ; ; ; ; ; ; ; ; ;
-	 		 "261C A284 3452 FB01 F6DF  6CF4 F9B7 864F 2AB4 6F18"))))
-	  %default-channels)
-	 )
+(service home-channels-service-type (load "channels.scm"))
 ;; Environment:3 ends here
 
 ;; [[file:Dotfiles.org::*Environment][Environment:4]]
