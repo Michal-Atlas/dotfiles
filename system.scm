7388a71f86fe1761cf9cf3485f0f2b0161430d31
Michal Atlas <michal_atlas+git@posteo.net>
2023-07-17 09:55:33 +0200

    Add self to Nix trusted users

--- system.scm
+++ system.scm
@@ -373,7 +373,8 @@
    (service nix-service-type
 	    (nix-configuration
 	     (extra-config
-	      '("experimental-features = nix-command flakes"))))
+	      '("experimental-features = nix-command flakes"
+                "trusted-users = @wheel"))))
    (service bluetooth-service-type)
    (modify-services %desktop-services
      (gdm-service-type
