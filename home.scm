a9ff551947e0408508f28d4dceee2473d6b4bf9c
Michal Atlas <michal_atlas+git@posteo.net>
2023-07-23 23:40:28 +0200

    Foot add padding

--- home.scm
+++ home.scm
@@ -400,13 +400,14 @@
              ;; local-file being explicit allows earlier check for file existence
              `((".emacs.d/init.el" ,(local-file "files/emacs.el"))
                (".config/foot/foot.ini"
-                (apply mixed-text-file "foot.ini"
-                       (serialize-ini-config
-                        `((main
-                           ((font . #{Fira Code:size=12}#)
-                            (include . ,(file-append
-                                         foot
-                                         "/share/foot/themes/monokai-pro"))))))))
+                ,(apply mixed-text-file "foot.ini"
+                        (serialize-ini-config
+                         `((main
+                            ((font . #{Fira Code:size=12}#)
+                             (pad . #{5x5 center}#)
+                             (include . ,(file-append
+                                          foot
+                                          "/share/foot/themes/monokai-pro"))))))))
                (".guile" ,(local-file "files/guile.scm"))
                (".sbclrc" ,(local-file "files/sbcl.lisp"))
                (".local/share/nyxt/bookmarks.lisp" ,(local-file "files/nyxt/bookmarks.lisp"))
