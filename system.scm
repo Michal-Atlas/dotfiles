c65f1b81e512f5a13395fb6abe1e54c13e413996
Michal Atlas <michal_atlas+git@posteo.net>
2023-07-23 02:36:49 +0200

    Added Cuirass to Hydra

--- system.scm
+++ system.scm
@@ -116,6 +116,26 @@
      (@host
       #:hydra
       (list
+       (service cuirass-service-type
+                (cuirass-configuration
+                 (interval (* 60 60))
+                 (host "0.0.0.0")
+                 (use-substitutes? #t)
+                 (specifications
+                  #~(list (specification
+                           (name "atlas")
+                           (build '(channels atlas))
+                           (channels
+                            (cons
+                             (channel
+                              (name 'atlas)
+                              (url "https://git.sr.ht/~michal_atlas/guix-channel")
+                              (introduction
+                               (make-channel-introduction
+                                "f0e838427c2d9c495202f1ad36cfcae86e3ed6af"
+                                (openpgp-fingerprint
+                                 "D45185A2755DAF831F1C3DC63EFBF2BBBB29B99E"))))
+                             %default-channels)))))))
        (service hurd-vm-service-type
         (hurd-vm-configuration
          (disk-size (* 16 (expt 2 30)))
