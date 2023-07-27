3c2a0b2799da1cf4e9850b55888e972500fe82a9
Michal Atlas <michal_atlas+git@posteo.net>
2023-07-27 23:28:48 +0200

    Remove beaver labs channel

--- home.scm
+++ home.scm
@@ -155,10 +155,6 @@
                        "257cebd587b66e4d865b3537a9a88cccd7107c95"
                        (openpgp-fingerprint
                         "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
-                    (channel
-                     (name 'beaver-labs)
-                     (url "https://gitlab.com/edouardklein/guix")
-                     (branch "beaverlabs"))
                     %default-channels))
     (home-sway
      (config
