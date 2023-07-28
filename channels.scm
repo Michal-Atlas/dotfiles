3553f2a8875519e2035b1cff449cf8c49734570f
Michal Atlas <michal_atlas+git@posteo.net>
2023-07-28 21:11:45 +0200

    Trying the channel thing again

--- channels.scm
+++ channels.scm
@@ -47,6 +47,9 @@
           "257cebd587b66e4d865b3537a9a88cccd7107c95"
           (openpgp-fingerprint
            "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
+       (channel
+        (name 'dotfiles)
+        (url "https://git.sr.ht/~michal_atlas/dotfiles"))
        (channel-with-substitutes-available
        %default-guix-channel
        "https://ci.guix.gnu.org")))
