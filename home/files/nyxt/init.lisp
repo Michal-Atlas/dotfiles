c693d45483a29e377a55cbaa07a1291f37a8dda4
Michal Atlas <michal_atlas+git@posteo.net>
2023-08-12 21:10:53 +0200

    New nyxt syntax

--- home/files/nyxt/init.lisp
+++ home/files/nyxt/init.lisp
@@ -1,5 +1,4 @@
-(defmethod customize-instance ((input-buffer input-buffer) &key)
-  (disable-modes* 'nyxt/mode/vi:vi-normal-mode input-buffer)
-  (enable-modes* 'nyxt/mode/emacs:emacs-mode input-buffer))
-(defmethod customize-instance ((browser browser) &key)
-  (setf (slot-value browser 'theme) theme:+dark-theme+))
+(define-configuration web-buffer
+  ((default-modes (pushnew 'nyxt/mode/emacs:emacs-mode %slot-value%))))
+(define-configuration browser
+  ((theme theme:+dark-theme+)))
