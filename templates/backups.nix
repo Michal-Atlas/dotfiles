93fe4a4393a099f49ee3bc0ef3b3938deae9307b
Michal Atlas <michal_atlas+git@posteo.net>
2023-09-06 16:23:26 +0200

    Replaced own autosnap with btrbk

--- templates/backups.nix
+++ templates/backups.nix
@@ -1,43 +1,16 @@
 { pkgs, ... }: {
-  systemd =
-    let
-      make-btrfs-timer = label: period: {
-        "btrfs-autosnap-${label}" = {
-          wantedBy = [ "timers.target" ];
-          timerConfig = {
-            OnCalendar = period;
-            Persistent = true;
-            Unit = "btrfs-autosnap-${label}.service";
-          };
+  services.btrbk.instances."btrbk" = {
+    settings = {
+      volume."/home" = {
+        subvolume."." = {
+          snapshot_dir = ".btrfs";
+          snapshot_create = "onchange";
+          timestamp_format = "long-iso";
+          snapshot_preserve_min = "latest";
+          snapshot_preserve = "25h 31d 4w 12m";
         };
       };
-      make-btrfs-service = label: path: retention: {
-        "btrfs-autosnap-${label}" = {
-          script = ''
-            ${pkgs.sbcl.withPackages (ps: with ps; [
-              clingon local-time pkgs.btrfs-progs
-            ])}/bin/sbcl \
-                --script ${../scripts/btrfs-autosnap.lisp} \
-                --label '${label}' \
-                --path '${path}' \
-                --retention '${builtins.toString retention}'
-          '';
-          serviceConfig = { Type = "oneshot"; };
-        };
-      };
-    in
-    {
-      timers =
-        (make-btrfs-timer "frequent" "*:0/15") //
-        (make-btrfs-timer "hourly" "hourly") //
-        (make-btrfs-timer "daily" "daily") //
-        (make-btrfs-timer "weekly" "weekly") //
-        (make-btrfs-timer "monthly" "monthly");
-      services =
-        (make-btrfs-service "frequent" "/home/" 4) //
-        (make-btrfs-service "hourly" "/home/" 26) //
-        (make-btrfs-service "daily" "/home/" 4) //
-        (make-btrfs-service "weekly" "/home/" 5) //
-        (make-btrfs-service "monthly" "/home/" 12);
     };
+    onCalendar = "hourly";
+  };
 }
