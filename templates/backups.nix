98138d13c6bfe5271a93dc9fda1783ee5772ec23
Michal Atlas <michal_atlas+git@posteo.net>
2023-09-06 16:37:50 +0200

    Creating frequent btrbk

--- templates/backups.nix
+++ templates/backups.nix
@@ -6,11 +6,11 @@
           snapshot_dir = ".btrfs";
           snapshot_create = "onchange";
           timestamp_format = "long-iso";
-          snapshot_preserve_min = "latest";
+          snapshot_preserve_min = "1h";
           snapshot_preserve = "25h 31d 4w 12m";
         };
       };
     };
-    onCalendar = "hourly";
+    onCalendar = "*:0/12";
   };
 }
