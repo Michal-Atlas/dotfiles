d3f16aeb77954c508a68fb3ad94c4c9c5c2ddeb7
Michal Atlas <michal_atlas+git@posteo.net>
2023-09-23 14:01:35 +0200

    Add names to mcron Jobs

--- home/services/mcron.scm
+++ home/services/mcron.scm
@@ -16,10 +16,12 @@
                   #~(job "0 6 * * *"
 	                 (string-append
 	                  #$(file-append onedrive "/bin/onedrive")
-	                  " --synchronize"))
+	                  " --synchronize")
+                         "Onedrive")
                   #~(job "0 * * * *"
 	                 (string-append
 	                  #$(file-append findutils "/bin/find")
-	                  " ~/tmp/ ~/Downloads/ -mindepth 1 -mtime +2 -delete;"))
+	                  " ~/tmp/ ~/Downloads/ -mindepth 1 -mtime +2 -delete;")
+                         "Clear tmpfiles")
                   #~(job "0 * * * *"
 	                 "guix gc -F 20G")))))))
