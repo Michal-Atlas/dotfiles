02bc2a8390773f084763add176e1502df2074bd1
Michal Atlas <michal_atlas+git@posteo.net>
2023-10-08 12:15:04 +0200

    Add night-theme-switcher

--- atlas/config/home/packages.scm
+++ atlas/config/home/packages.scm
@@ -205,26 +205,27 @@
 
 (define %gnome-packages
   (hm/packages
-   gnome-shell-extension-vitals
+   gnome-shell-extension-appindicator
+   gnome-shell-extension-blur-my-shell
+   gnome-shell-extension-burn-my-windows
+   gnome-shell-extension-clipboard-indicator
+   gnome-shell-extension-customize-ibus
+   gnome-shell-extension-dash-to-dock
+   gnome-shell-extension-dash-to-panel
+   gnome-shell-extension-gsconnect
+   gnome-shell-extension-hide-app-icon
+   gnome-shell-extension-jiggle
+   gnome-shell-extension-just-perfection
+   gnome-shell-extension-night-theme-switcher
+   gnome-shell-extension-noannoyance
+   gnome-shell-extension-paperwm
+   gnome-shell-extension-radio
+   gnome-shell-extension-sound-output-device-chooser
+   gnome-shell-extension-topicons-redux
+   gnome-shell-extension-transparent-window
    gnome-shell-extension-unite-shell
    gnome-shell-extension-vertical-overview
-   gnome-shell-extension-clipboard-indicator
-   gnome-shell-extension-topicons-redux
-   gnome-shell-extension-radio
-   gnome-shell-extension-paperwm
-   gnome-shell-extension-hide-app-icon
-   gnome-shell-extension-dash-to-panel
-   gnome-shell-extension-appindicator
-   gnome-shell-extension-customize-ibus
-   gnome-shell-extension-transparent-window
-   gnome-shell-extension-sound-output-device-chooser
-   gnome-shell-extension-just-perfection
-   gnome-shell-extension-gsconnect
-   gnome-shell-extension-dash-to-dock
-   gnome-shell-extension-burn-my-windows
-   gnome-shell-extension-blur-my-shell
-   gnome-shell-extension-jiggle
-   gnome-shell-extension-noannoyance))
+   gnome-shell-extension-vitals))
 
 (define %icon-packages
   (hm/packages
