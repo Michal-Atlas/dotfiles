83fa95adb7460849f8bd62fdb5157f7f93c0d756
Michal Atlas <michal_atlas+git@posteo.net>
2023-10-15 22:51:36 +0200

    Add unison job

--- atlas/config/home/files.scm
+++ atlas/config/home/files.scm
@@ -8,42 +8,46 @@
   #:use-module (gnu packages terminals))
 
 (define-public %files
-  (hm/+s home-files
-         ;; local-file being explicit allows earlier check for file existence
-         `((".config/guix/source" ,(local-file (getenv "DOTFILE_ROOT")
-                                               #:recursive? #t))
-           (".emacs.d/init.el" ,(local-file "files/emacs.el"))
-           (".config/foot/foot.ini"
-            ,(apply mixed-text-file "foot.ini"
-                    (serialize-ini-config
-                     `((main
-                        ((font . #{Fira Code:size=12}#)
-                         (pad . #{5x5 center}#)
-                         (include . ,(file-append
-                                      foot
-                                      "/share/foot/themes/monokai-pro"))))
-                       (colors
-                        ((alpha . 0.9)))))))
+  (compose
+   (hm/+s home-files
+          ;; local-file being explicit allows earlier check for file existence
+          `((".emacs.d/init.el" ,(local-file "../../../files/emacs.el"))
+            (".config/foot/foot.ini"
+             ,(apply mixed-text-file "foot.ini"
+                     (serialize-ini-config
+                      `((main
+                         ((font . #{Fira Code:size=12}#)
+                          (pad . #{5x5 center}#)
+                          (include . ,(file-append
+                                       foot
+                                       "/share/foot/themes/monokai-pro"))))
+                        (colors
+                         ((alpha . 0.9)))))))
 
-           (".guile" ,(local-file "files/guilerc"))
-           (".sbclrc" ,(plain-file "sbclrc" "(load #p\"~/.sbclrc.lisp\")"))
-           (".inputrc" ,(local-file "files/inputrc"))
-           (".gtkrc-2.0" ,(local-file "files/gtk2.ini"))
-           (".local/share/nyxt/bookmarks.lisp" ,(local-file "files/nyxt/bookmarks.lisp"))
-           (".config/mpv/scripts/mpris.so"
-            ,(file-append mpv-mpris "/lib/mpris.so"))
-           (".face"
-            ,(file-fetch
-              "https://git.sr.ht/~michal_atlas/www/blob/0d74b8802a0c9e820c38e41cfe920f1bdbb06746/assets/mlxan/carcass-mountain/portraits/weeping-priest.jpeg"
-              "1MnHsYKJpI/h8zJa01MtPjgf/UMtSLZQ7YWcslj1Xxc="))
-           (".bin/pp"
-            ,(program-file "pp"
-                           #~(begin
-                               (use-modules (ice-9 pretty-print))
+            (".guile" ,(local-file "../../../files/guilerc"))
+            (".sbclrc" ,(plain-file "sbclrc" "(load #p\"~/.sbclrc.lisp\")"))
+            (".inputrc" ,(local-file "../../../files/inputrc"))
+            (".gtkrc-2.0" ,(local-file "../../../files/gtk2.ini"))
+            (".local/share/nyxt/bookmarks.lisp" ,(local-file "../../../files/nyxt/bookmarks.lisp"))
+            (".config/mpv/scripts/mpris.so"
+             ,(file-append mpv-mpris "/lib/mpris.so"))
+            (".face"
+             ,(file-fetch
+               "https://git.sr.ht/~michal_atlas/www/blob/0d74b8802a0c9e820c38e41cfe920f1bdbb06746/assets/mlxan/carcass-mountain/portraits/weeping-priest.jpeg"
+               "1MnHsYKJpI/h8zJa01MtPjgf/UMtSLZQ7YWcslj1Xxc="))
+            (".bin/pp"
+             ,(program-file "pp"
+                            #~(begin
+                                (use-modules (ice-9 pretty-print))
 
-                               (call-with-input-file (cadr (command-line))
-                                 (lambda (f)
-                                   (let loop ((r (read f)))
-                                     (unless (eof-object? r)
-                                       (pretty-print r)
-                                       (loop (read f))))))))))))
+                                (call-with-input-file (cadr (command-line))
+                                  (lambda (f)
+                                    (let loop ((r (read f)))
+                                      (unless (eof-object? r)
+                                        (pretty-print r)
+                                        (loop (read f)))))))))))
+   (maybe-service
+    (const (string= (gethostname) "hydra"))
+    (hm/+s home-files
+           `((".unison/default.prf"
+              ,(local-file "../../../files/default.prf")))))))
--- atlas/config/home/services/mcron.scm
+++ atlas/config/home/services/mcron.scm
@@ -4,22 +4,30 @@
   #:use-module (gnu home services mcron)
   #:use-module (gnu packages base)
   #:use-module (gnu packages sync)
-  #:use-module (gnu packages ocaml))
+  #:use-module (gnu packages ocaml)
+  #:use-module (gnu packages linux))
 
 (define-public %mcron
   (compose
    (maybe-service
     (const (string= (gethostname) "hydra"))
-    (hm/&s home-mcron
-           (jobs
-            (list #~(job "0 6 * * *"
-	                 (string-append
-	                  #$(file-append onedrive "/bin/onedrive")
-	                  " --synchronize")
-                         "Onedrive")))))
+    (hm/+s home-mcron
+           (home-mcron-configuration
+            (jobs
+             (list #~(job "0 6 * * *"
+                          (string-append
+                           #$(file-append onedrive "/bin/onedrive")
+                           " --synchronize")
+                          "Onedrive")
+                   #~(job "*/10 * * * *"
+                          (string-append
+                           #$(file-append procps "/bin/pgrep") " unison >/dev/null || "
+                           #$(file-append unison "/bin/unison")
+                           " -batch")
+                          "Unison"))))))
    (hm/&s home-mcron
           (jobs
-           (list 
+           (list
             #~(job "0 * * * *"
                    (string-append
                     #$(file-append findutils "/bin/find")
