e6469cab3a447b8410d82e919331ea660935354d
Michal Atlas <michal_atlas+git@posteo.net>
2023-10-15 22:56:44 +0200

    Cuirass only on Hydra

--- atlas/config/system/services.scm
+++ atlas/config/system/services.scm
@@ -33,34 +33,34 @@
 
 (define-public %services
   (compose
-   (if-host "hydra" (&s hurd-vm))
-   (&s cuirass
-       (interval (* 24 60 60))
-       (host "0.0.0.0")
-       (use-substitutes? #t)
-       (specifications
-        #~(list (specification
-                 (name "atlas")
-                 (build '(channels atlas))
-                 (channels
-                  (cons
-                   (channel
-                    (name 'atlas)
-                    (url "https://git.sr.ht/~michal_atlas/guix-channel")
-                    (introduction
-                     (make-channel-introduction
-                      "f0e838427c2d9c495202f1ad36cfcae86e3ed6af"
-                      (openpgp-fingerprint
-                       "D45185A2755DAF831F1C3DC63EFBF2BBBB29B99E"))))
-                   %default-channels))))))
    (if-host "hydra"
+            (&s hurd-vm)
+            (&s cuirass
+                (interval (* 24 60 60))
+                (host "0.0.0.0")
+                (use-substitutes? #t)
+                (specifications
+                 #~(list (specification
+                          (name "atlas")
+                          (build '(channels atlas))
+                          (channels
+                           (cons
+                            (channel
+                             (name 'atlas)
+                             (url "https://git.sr.ht/~michal_atlas/guix-channel")
+                             (introduction
+                              (make-channel-introduction
+                               "f0e838427c2d9c495202f1ad36cfcae86e3ed6af"
+                               (openpgp-fingerprint
+                                "D45185A2755DAF831F1C3DC63EFBF2BBBB29B99E"))))
+                            %default-channels))))))
             (&s quassel)
             (&s tes3mp-server))
    (&s pam-limits #:config
        (list
         (pam-limits-entry "*" 'both 'nofile 524288)))
 
-   (+s hosts
+   (+s hosts yggdrasil-hosts
        (list (host (string-join '("201" "a50e" "ca2d" "72bf"
                                   "89aa" "e12" "e14d" "f2e6") ":") "hydra")
              (host (string-join '("200" "ac59" "de15" "abe5"
