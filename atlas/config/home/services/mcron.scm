7904fd38aa68e0dbcc12fb599f4931dfd623ab07
Michal Atlas <michal_atlas+git@posteo.net>
2023-10-16 00:11:31 +0200

    Mcron extension only expects jobs

--- atlas/config/home/services/mcron.scm
+++ atlas/config/home/services/mcron.scm
@@ -12,19 +12,17 @@
    (maybe-service
     (const (string= (gethostname) "hydra"))
     (hm/+s home-mcron hydra-extensions
-           (home-mcron-configuration
-            (jobs
-             (list #~(job "0 6 * * *"
-                          (string-append
-                           #$(file-append onedrive "/bin/onedrive")
-                           " --synchronize")
-                          "Onedrive")
-                   #~(job "*/10 * * * *"
-                          (string-append
-                           #$(file-append procps "/bin/pgrep") " unison >/dev/null || "
-                           #$(file-append unison "/bin/unison")
-                           " -batch")
-                          "Unison"))))))
+           (list #~(job "0 6 * * *"
+                        (string-append
+                         #$(file-append onedrive "/bin/onedrive")
+                         " --synchronize")
+                        "Onedrive")
+                 #~(job "*/10 * * * *"
+                        (string-append
+                         #$(file-append procps "/bin/pgrep") " unison >/dev/null || "
+                         #$(file-append unison "/bin/unison")
+                         " -batch")
+                        "Unison"))))
    (hm/&s home-mcron
           (jobs
            (list
