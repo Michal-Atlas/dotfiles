575229eb189db7e28009d58bfe9fb6b5a431cbee
Michal Atlas <michal_atlas+git@posteo.net>
2023-10-21 19:14:16 +0200

    Reverse filesystems to be in order

--- atlas/config/system/filesystems.scm
+++ atlas/config/system/filesystems.scm
@@ -23,78 +23,80 @@
   (compose
    %swap
    (if-host "dagon"
-            (file-systems
-             (file-system
-              (mount-point "/boot/efi")
-              (device (uuid "D762-6C63" 'fat32))
-              (type "vfat")))
-            ((mapped-file-systems
-              (rpool "home")
-              (mapped-device
-               (source "/dev/mapper/rpool-home")
-               (target "crypthome")
-               (type luks-device-mapping)))
-             (file-system
-              (mount-point "/home")
-              (device "/dev/mapper/crypthome")
-              (type "btrfs")))
-            ((mapped-file-systems (rpool "root"))
-             (file-system
-              (mount-point "/")
-              (device "/dev/mapper/rpool-root")
-              (type "btrfs")
-              (options
-               (alist->file-system-options `(("subvol" . "@guix")
-                                             ,compress))))))
+            (rev
+             (file-systems
+              (file-system
+               (mount-point "/boot/efi")
+               (device (uuid "D762-6C63" 'fat32))
+               (type "vfat")))
+             ((mapped-file-systems
+               (rpool "home")
+               (mapped-device
+                (source "/dev/mapper/rpool-home")
+                (target "crypthome")
+                (type luks-device-mapping)))
+              (file-system
+               (mount-point "/home")
+               (device "/dev/mapper/crypthome")
+               (type "btrfs")))
+             ((mapped-file-systems (rpool "root"))
+              (file-system
+               (mount-point "/")
+               (device "/dev/mapper/rpool-root")
+               (type "btrfs")
+               (options
+                (alist->file-system-options `(("subvol" . "@guix")
+                                              ,compress)))))))
    (if-host "hydra"
-            (file-systems
-             (file-system
-              (mount-point "/boot/efi")
-              (device (file-system-label "EFIBOOT"))
-              (type "vfat")))
-            ((mapped-file-systems
-              (spool "root"))
-             (file-system
-              (mount-point "/")
-              (device "/dev/mapper/spool-root")
-              (options
-               (alist->file-system-options `(("subvol" . "@guix")
-                                             ,compress)))
-              (type "btrfs")))
-            ((mapped-file-systems
-              (rpool "home"))
-             (file-system
-              (mount-point "/home")
-              (device "/dev/mapper/rpool-home")
-              (options
-               (alist->file-system-options `(("subvol" . "@home")
-                                             ,compress)))
-              (type "btrfs")))
-            ((mapped-file-systems
-              (rpool "vault"))
-             (file-system
-              (mount-point "/home/michal_atlas/tmp")
-              (device "/dev/mapper/rpool-vault")
-              (options
-               (alist->file-system-options '(("subvol" . "@tmp"))))
-              (type "btrfs"))
-             (file-system
-              (mount-point "/home/michal_atlas/Downloads")
-              (device "/dev/mapper/rpool-vault")
-              (options
-               (alist->file-system-options '(("subvol" . "@tmp"))))
-              (type "btrfs"))
-             (file-system
-              (mount-point "/home/michal_atlas/Games")
-              (device "/dev/mapper/rpool-vault")
-              (options
-               (alist->file-system-options `(("subvol" . "@games")
-                                             ,compress)))
-              (type "btrfs"))
-             (file-system
-              (mount-point "/var/lib/transmission-daemon/downloads/")
-              (device "/dev/mapper/rpool-vault")
-              (options
-               (alist->file-system-options `(("subvol" . "@torrents")
-                                             ,compress)))
-              (type "btrfs"))))))
+            (rev
+             (file-systems
+              (file-system
+               (mount-point "/boot/efi")
+               (device (file-system-label "EFIBOOT"))
+               (type "vfat")))
+             ((mapped-file-systems
+               (spool "root"))
+              (file-system
+               (mount-point "/")
+               (device "/dev/mapper/spool-root")
+               (options
+                (alist->file-system-options `(("subvol" . "@guix")
+                                              ,compress)))
+               (type "btrfs")))
+             ((mapped-file-systems
+               (rpool "home"))
+              (file-system
+               (mount-point "/home")
+               (device "/dev/mapper/rpool-home")
+               (options
+                (alist->file-system-options `(("subvol" . "@home")
+                                              ,compress)))
+               (type "btrfs")))
+             ((mapped-file-systems
+               (rpool "vault"))
+              (file-system
+               (mount-point "/home/michal_atlas/tmp")
+               (device "/dev/mapper/rpool-vault")
+               (options
+                (alist->file-system-options '(("subvol" . "@tmp"))))
+               (type "btrfs"))
+              (file-system
+               (mount-point "/home/michal_atlas/Downloads")
+               (device "/dev/mapper/rpool-vault")
+               (options
+                (alist->file-system-options '(("subvol" . "@tmp"))))
+               (type "btrfs"))
+              (file-system
+               (mount-point "/home/michal_atlas/Games")
+               (device "/dev/mapper/rpool-vault")
+               (options
+                (alist->file-system-options `(("subvol" . "@games")
+                                              ,compress)))
+               (type "btrfs"))
+              (file-system
+               (mount-point "/var/lib/transmission-daemon/downloads/")
+               (device "/dev/mapper/rpool-vault")
+               (options
+                (alist->file-system-options `(("subvol" . "@torrents")
+                                              ,compress)))
+               (type "btrfs")))))))
