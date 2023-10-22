73f19df5949f255031e7b771f035a406a9f8e74e
Michal Atlas <michal_atlas+git@posteo.net>
2023-10-22 16:18:07 +0200

    Properly declare filesystem dependencies

--- atlas/config/system/filesystems.scm
+++ atlas/config/system/filesystems.scm
@@ -9,9 +9,12 @@
 
 (define %swap
   (compose
-   (if-host "dagon"
-            ((mapped-swap-devices (rpool "swap"))
-             (swap-space (target "/dev/mapper/rpool-swap"))))
+   (let ((rpool-swap (rpool "swap")))
+    (if-host "dagon"
+             (swap-devices
+              (swap-space (target "/dev/mapper/rpool-swap")
+                          (dependencies (list rpool-swap))))
+             (mapped-devices rpool-swap)))
    (if-host "hydra"
             (swap-devices
              (swap-space (target (file-system-label "SWAP")))))))
@@ -22,81 +25,97 @@
 (define-public %filesystems
   (compose
    %swap
-   (if-host "dagon"
-            (rev
-             (file-systems
-              (file-system
-               (mount-point "/boot/efi")
-               (device (uuid "D762-6C63" 'fat32))
-               (type "vfat")))
-             ((mapped-file-systems
-               (rpool "home")
-               (mapped-device
-                (source "/dev/mapper/rpool-home")
-                (target "crypthome")
-                (type luks-device-mapping)))
-              (file-system
-               (mount-point "/home")
-               (device "/dev/mapper/crypthome")
-               (type "btrfs")))
-             ((mapped-file-systems (rpool "root"))
+
+   (let* ((rpool-root (rpool "root"))
+          (rpool-home (rpool "home"))
+          (unlock-home
+           (mapped-device
+            (source "/dev/mapper/rpool-home")
+            (target "crypthome")
+            (type luks-device-mapping)))
+          (root
               (file-system
                (mount-point "/")
                (device "/dev/mapper/rpool-root")
                (type "btrfs")
                (options
                 (alist->file-system-options `(("subvol" . "@guix")
-                                              ,compress)))))))
-   (if-host "hydra"
-            (rev
-             (file-systems
-              (file-system
-               (mount-point "/boot/efi")
-               (device (file-system-label "EFIBOOT"))
-               (type "vfat")))
-             ((mapped-file-systems
-               (spool "root"))
+                                              ,compress)))
+               (dependencies (list rpool-root))))
+          (efi
+           (file-system
+            (mount-point "/boot/efi")
+            (device (uuid "D762-6C63" 'fat32))
+            (type "vfat")))
+          (home
+           (file-system
+            (mount-point "/home")
+            (device "/dev/mapper/crypthome")
+            (type "btrfs"))))
+     (if-host "dagon"
+              (mapped-devices
+               unlock-home rpool-home rpool-root)
+              (file-systems
+               efi root home)))
+
+   (let* ((spool-root (spool "root"))
+          (root
               (file-system
                (mount-point "/")
                (device "/dev/mapper/spool-root")
                (options
                 (alist->file-system-options `(("subvol" . "@guix")
                                               ,compress)))
-               (type "btrfs")))
-             ((mapped-file-systems
-               (rpool "home"))
-              (file-system
-               (mount-point "/home")
-               (device "/dev/mapper/rpool-home")
-               (options
-                (alist->file-system-options `(("subvol" . "@home")
-                                              ,compress)))
-               (type "btrfs")))
-             ((mapped-file-systems
-               (rpool "vault"))
-              (file-system
-               (mount-point "/home/michal_atlas/tmp")
-               (device "/dev/mapper/rpool-vault")
-               (options
-                (alist->file-system-options '(("subvol" . "@tmp"))))
-               (type "btrfs"))
-              (file-system
-               (mount-point "/home/michal_atlas/Downloads")
-               (device "/dev/mapper/rpool-vault")
-               (options
-                (alist->file-system-options '(("subvol" . "@tmp"))))
-               (type "btrfs"))
-              (file-system
-               (mount-point "/home/michal_atlas/Games")
-               (device "/dev/mapper/rpool-vault")
-               (options
-                (alist->file-system-options `(("subvol" . "@games")
-                                              ,compress)))
-               (type "btrfs"))
-              (file-system
-               (mount-point "/var/lib/transmission-daemon/downloads/")
-               (device "/dev/mapper/rpool-vault")
-               (options
-                (alist->file-system-options `(("subvol" . "@torrents")
-                                              ,compress)))
-               (type "btrfs")))))))
+               (type "btrfs")
+               (dependencies (list spool-root))))
+          (efi
+           (file-system
+            (mount-point "/boot/efi")
+            (device (file-system-label "EFIBOOT"))
+            (type "vfat")))
+          (rpool-home (rpool "home"))
+          (home
+           (file-system
+            (mount-point "/home")
+            (device "/dev/mapper/rpool-home")
+            (options
+             (alist->file-system-options `(("subvol" . "@home")
+                                           ,compress)))
+            (type "btrfs")
+            (dependencies (list rpool-home))))
+          (rpool-vault (rpool "vault")))
+     (if-host "hydra"
+              (file-systems
+               efi root home
+               (file-system
+                (mount-point "/home/michal_atlas/tmp")
+                (device "/dev/mapper/rpool-vault")
+                (options
+                 (alist->file-system-options '(("subvol" . "@tmp"))))
+                (type "btrfs")
+                (dependencies (list home rpool-vault)))
+               (file-system
+                (mount-point "/home/michal_atlas/Downloads")
+                (device "/dev/mapper/rpool-vault")
+                (options
+                 (alist->file-system-options '(("subvol" . "@tmp"))))
+                (type "btrfs")
+                (dependencies (list home rpool-vault)))
+               (file-system
+                (mount-point "/home/michal_atlas/Games")
+                (device "/dev/mapper/rpool-vault")
+                (options
+                 (alist->file-system-options `(("subvol" . "@games")
+                                               ,compress)))
+                (type "btrfs")
+                (dependencies (list home rpool-vault)))
+               (file-system
+                (mount-point "/var/lib/transmission-daemon/downloads/")
+                (device "/dev/mapper/rpool-vault")
+                (options
+                 (alist->file-system-options `(("subvol" . "@torrents")
+                                               ,compress)))
+                (type "btrfs")
+                (dependencies (list rpool-vault))))
+              (mapped-devices
+               rpool-vault rpool-home spool-root)))))
