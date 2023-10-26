2aab547603fc45b7f67993898748ed663bcf8c9d
Michal Atlas <michal_atlas+git@posteo.net>
2023-10-27 00:04:02 +0200

    Swaylock wallpaper

--- atlas/config/home/services/wm/misc.scm
+++ atlas/config/home/services/wm/misc.scm
@@ -1,7 +1,14 @@
 (define-module (atlas config home services wm misc)
   #:use-module (atlas combinators)
+  #:use-module (atlas utils download)
+  #:use-module (guix gexp)
   #:use-module (rde home services wm)
   #:use-module (guixrus home services mako))
+
+(define lock-wallpaper
+  (file-fetch
+   "https://www.gnu.org/graphics/techy-gnu-tux-bivouac-large.jpg"
+   "oOYE1PZ1g2HvXqUbL5aE3xkW+SqkKPmO0GaVI9QlI40="))
 
 (define-public %misc
   (compose
@@ -13,7 +20,8 @@
 
    (hm/&s home-swaylock
           (config
-           `((color . "000000"))))
+           `((color . "000000")
+             ,#~(string-append "image=" #$lock-wallpaper))))
 
    (hm/&s home-mako
           (sections
