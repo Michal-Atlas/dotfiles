47df612b1387d2e6b492a95cf10133b6bd2304df
Michal Atlas <michal_atlas+git@posteo.net>
2023-11-26 00:08:45 +0100

    Wireguard: Add IPv6

--- system/networks/wireguard.scm
+++ system/networks/wireguard.scm
@@ -17,7 +17,7 @@
    (name "central")
    (endpoint "130.61.54.212:51820")
    (public-key "DfSW8vkOq5riNSenbVwRK7GUECcfbvGSUjHLUw1PAhc=")
-   (allowed-ips '("13.0.0.0/24"))
+   (allowed-ips '("13.0.0.0/24" "fd4c:16e4:7d9b:0::/48"))
    (keep-alive (wireguard:keepalive))))
 
 (define wireguard:peers
@@ -25,21 +25,21 @@
    (list
     (wireguard-peer
      (name "hydra")
-     (allowed-ips '("13.0.0.2/32"))
+     (allowed-ips '("13.0.0.2/32" "fd4c:16e4:7d9b:0::2/64"))
      (public-key "0MigA4ewwzbwrlrZsi7+xhxn893q3nbtTPn6uiB2LEE="))
     (wireguard-peer
      (name "dagon")
-     (allowed-ips '("13.0.0.3/32"))
+     (allowed-ips '("13.0.0.3/32" "fd4c:16e4:7d9b:0::3/64"))
      (public-key "VUk71x+wmwt//38RNT47ZNFJP0ZB2xB++4bAAtT6uEU="))
     (wireguard-peer
      (name "arc")
-     (allowed-ips '("13.0.0.4/32"))
+     (allowed-ips '("13.0.0.4/32" "fd4c:16e4:7d9b:0::4/64"))
      (public-key "+i1Pv+p34kp/iEsPYeIj1mz/WkisdAUfQYlioRLbmxY=")))))
 
 (define (remote-config-file)
   ((@@ (gnu services vpn) wireguard-configuration-file)
    (wireguard-configuration
-    (addresses (list "13.0.0.1/24"))
+    (addresses (list "13.0.0.1/24" "fd4c:16e4:7d9b:0::1/64"))
     (peers (wireguard:peers)))))
 
 (define (peer-by-name)
@@ -53,11 +53,14 @@
 (define (wireguard:get)
   (list
    (+s hosts wireguard-hosts
-       (map
+       (append-map
         (lambda (wp)
-          (host
-           (car (string-split (car (wireguard-peer-allowed-ips wp)) #\/))
-           (string-append "wg-" (wireguard-peer-name wp))))
+          (map
+           (lambda (addr)
+             (host
+              (car (string-split addr #\/))
+              (string-append "wg-" (wireguard-peer-name wp))))
+           (wireguard-peer-allowed-ips wp)))
         (wireguard:peers)))
    (&s wireguard
        (addresses
