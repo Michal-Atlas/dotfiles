6f8dd4a0fdd9a8837f856cbaa2d6a7df5c28869e
Michal Atlas <michal_atlas+git@posteo.net>
2023-12-07 21:11:30 +0100

    home-nix use new service

--- home/nix.scm
+++ home/nix.scm
@@ -1,10 +1,10 @@
 (define-module (home nix)
   #:use-module (atlas utils services)
   #:use-module (atlas home services nix)
+  #:use-module (guix gexp)
   #:export (nix))
 
 (define nix
- (&s home-nix #:config
-     (string-append
-      (getenv "DOTFILE_ROOT")
-      "/nix")))
+ (&s home-nix
+     (flake
+      (local-file "../nix" "nix-home" #:recursive? #t))))
