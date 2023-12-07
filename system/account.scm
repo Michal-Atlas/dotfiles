1671e15dbdd740d803f1077e037d3c6af86a3b5f
Michal Atlas <michal_atlas+git@posteo.net>
2023-12-07 21:20:37 +0100

    Remove transmission account

--- system/account.scm
+++ system/account.scm
@@ -13,4 +13,4 @@
         (home-directory "/home/michal_atlas")
         (supplementary-groups
          '("wheel" "netdev" "audio"
-           "video" "libvirt" "kvm" "tty" "transmission"))))))
+           "video" "libvirt" "kvm" "tty"))))))
