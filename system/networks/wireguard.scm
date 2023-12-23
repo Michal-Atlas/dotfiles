cfb5c427a487b985d15c3f65e05c8615e56853d7
Michal Atlas <michal_atlas+git@posteo.net>
2023-12-23 18:40:21 +0100

    Change Wireguard address subnet

--- system/networks/wireguard.scm
+++ system/networks/wireguard.scm
@@ -17,7 +17,7 @@
    (name "central")
    (endpoint "130.61.54.212:51820")
    (public-key "DfSW8vkOq5riNSenbVwRK7GUECcfbvGSUjHLUw1PAhc=")
-   (allowed-ips '("13.0.0.0/24"))
+   (allowed-ips '("10.0.0.0/24"))
    (keep-alive (wireguard:keepalive))))
 
 (define wireguard:peers
@@ -25,25 +25,25 @@
    (list
     (wireguard-peer
      (name "hydra")
-     (allowed-ips '("13.0.0.2/32"))
+     (allowed-ips '("10.0.0.2/32"))
      (public-key "0MigA4ewwzbwrlrZsi7+xhxn893q3nbtTPn6uiB2LEE="))
     (wireguard-peer
      (name "dagon")
-     (allowed-ips '("13.0.0.3/32"))
+     (allowed-ips '("10.0.0.3/32"))
      (public-key "VUk71x+wmwt//38RNT47ZNFJP0ZB2xB++4bAAtT6uEU="))
     (wireguard-peer
      (name "arc")
-     (allowed-ips '("13.0.0.4/32"))
      (public-key "+i1Pv+p34kp/iEsPYeIj1mz/WkisdAUfQYlioRLbmxY="))
     (wireguard-peer
      (name "sps")
      (allowed-ips '("13.0.0.101/32" "13.0.0.102/32" "13.0.0.103/32"))
      (public-key "h5aakFQU0KsraXu6o6ySX7JY588sBt6IV+GiaRMjCEw=")))))
+     (allowed-ips '("10.0.0.4/32"))
 
 (define (remote-config-file)
   ((@@ (gnu services vpn) wireguard-configuration-file)
    (wireguard-configuration
-    (addresses (list "13.0.0.1/24"))
+    (addresses (list "10.0.0.1/24"))
     (peers (wireguard:peers)))))
 
 (define (peer-by-name)
