6a6acac77461d50d685998d46dba47f1c48f92f8
Michal Atlas <michal_atlas+git@posteo.net>
2024-01-06 20:02:32 +0100

    Add sops channel

--- channels.scm
+++ channels.scm
@@ -16,6 +16,16 @@
        (openpgp-fingerprint
         "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
     (channel
+     (name 'sops-guix)
+     (url "https://git.sr.ht/~fishinthecalculator/sops-guix")
+     (branch "main")
+     ;; Enable signature verification:
+     (introduction
+      (make-channel-introduction
+       "0bbaf1fdd25266c7df790f65640aaa01e6d2dbc9"
+       (openpgp-fingerprint
+        "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
+    (channel
      (name 'emacs)
      (url "https://github.com/babariviere/guix-emacs")
      (introduction
