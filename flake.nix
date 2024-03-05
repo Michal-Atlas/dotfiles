0724c61c444735a13866718731b87b713a535ff2
Michal Atlas <michal_atlas+git@posteo.net>
2024-03-05 17:48:37 +0100

    build(flake.nix): Use flake-parts

--- flake.nix
+++ flake.nix
@@ -16,6 +16,8 @@
       url = "github:StevenBlack/hosts";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
   };
 
   outputs = {
@@ -26,82 +28,92 @@
     nur,
     pre-commit-hooks,
     stevenblackhosts,
+    flake-parts,
+    systems,
     ...
-  } @ attrs: let
-    system = "x86_64-linux";
-    pkgs = (import nixpkgs) {inherit system;};
-  in {
-    nixosConfigurations = let
-      makeSys = file:
-        nixpkgs.lib.nixosSystem {
-          specialArgs =
-            attrs
-            // {
-              lib = nixpkgs.lib // import ./lib;
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;}
+    {
+      systems = import systems;
+      flake = {
+        nixosConfigurations = let
+          makeSys = file:
+            nixpkgs.lib.nixosSystem {
+              specialArgs =
+                inputs
+                // {
+                  lib = nixpkgs.lib // import ./lib;
+                };
+              modules = [
+                file
+                home-manager.nixosModules.home-manager
+                {
+                  home-manager = {
+                    extraSpecialArgs = inputs;
+                    useGlobalPkgs = true;
+                    useUserPackages = true;
+                    users.michal_atlas = import ./home;
+                  };
+                }
+                agenix.nixosModules.default
+                nur.nixosModules.nur
+                stevenblackhosts.nixosModule
+              ];
             };
-          modules = [
-            file
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                extraSpecialArgs = attrs;
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.michal_atlas = import ./home;
-              };
-            }
-            agenix.nixosModules.default
-            nur.nixosModules.nur
-            stevenblackhosts.nixosModule
-          ];
+        in {
+          hydra = makeSys ./system/machines/hydra;
+          dagon = makeSys ./system/machines/dagon;
         };
-    in {
-      hydra = makeSys ./system/machines/hydra;
-      dagon = makeSys ./system/machines/dagon;
-    };
+      };
+      perSystem = {
+        system,
+        pkgs,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = let
+            rebuild-cmd = cmd: ''nixos-rebuild ${cmd} --flake . "$@";'';
+            sudo-rebuild-cmd = cmd: "sudo ${rebuild-cmd cmd}";
+          in [
+            (pkgs.writeShellScriptBin "recon" (sudo-rebuild-cmd "switch"))
+            (pkgs.writeShellScriptBin "recboot" (sudo-rebuild-cmd "boot"))
+            (pkgs.writeShellScriptBin "check" "nix flake check $@;")
+            (pkgs.writeShellScriptBin "build" (rebuild-cmd "build"))
+            agenix.packages.${system}.default
+            (pkgs.writeShellScriptBin "recdiff" ''
+              build && ${pkgs.nix-diff}/bin/nix-diff /run/current-system result && rm result
+            '')
+            pkgs.commitizen
+          ];
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+        };
 
-    checks.${system} = {
-      pre-commit-check = pre-commit-hooks.lib.${system}.run {
-        src = ./.;
-        hooks = {
-          alejandra.enable = true;
-          shellcheck.enable = true;
-          shfmt.enable = true;
-          nil.enable = true;
-          deadnix.enable = true;
-          commitizen.enable = true;
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              alejandra.enable = true;
+              shellcheck.enable = true;
+              shfmt.enable = true;
+              nil.enable = true;
+              deadnix.enable = true;
+              commitizen.enable = true;
 
-          elisp-autofmt = {
-            enable = true;
-            name = "Elisp Autofmt";
-            entry = "${
-              (pkgs.writeShellScriptBin "autofmt.sh" ''
-                ${
-                  (pkgs.emacsPackagesFor pkgs.emacs).emacsWithPackages
-                  (epkgs: with epkgs; [elisp-autofmt])
-                }/bin/emacs "$1" --batch --eval "(require 'elisp-autofmt)" -f elisp-autofmt-buffer -f save-buffer --kill'')
-            }/bin/autofmt.sh";
-            files = "\\.(el)$";
+              elisp-autofmt = {
+                enable = true;
+                name = "Elisp Autofmt";
+                entry = "${
+                  (pkgs.writeShellScriptBin "autofmt.sh" ''
+                    ${
+                      (pkgs.emacsPackagesFor pkgs.emacs).emacsWithPackages
+                      (epkgs: with epkgs; [elisp-autofmt])
+                    }/bin/emacs "$1" --batch --eval "(require 'elisp-autofmt)" -f elisp-autofmt-buffer -f save-buffer --kill'')
+                }/bin/autofmt.sh";
+                files = "\\.(el)$";
+              };
+            };
           };
         };
       };
     };
-    devShells.${system}.default = nixpkgs.legacyPackages.${system}.mkShell {
-      nativeBuildInputs = let
-        rebuild-cmd = cmd: ''nixos-rebuild ${cmd} --flake . "$@";'';
-        sudo-rebuild-cmd = cmd: "sudo ${rebuild-cmd cmd}";
-      in [
-        (pkgs.writeShellScriptBin "recon" (sudo-rebuild-cmd "switch"))
-        (pkgs.writeShellScriptBin "recboot" (sudo-rebuild-cmd "boot"))
-        (pkgs.writeShellScriptBin "check" "nix flake check $@;")
-        (pkgs.writeShellScriptBin "build" (rebuild-cmd "build"))
-        agenix.packages.${system}.default
-        (pkgs.writeShellScriptBin "recdiff" ''
-          build && ${pkgs.nix-diff}/bin/nix-diff /run/current-system result && rm result
-        '')
-        pkgs.commitizen
-      ];
-      inherit (self.checks.${system}.pre-commit-check) shellHook;
-    };
-  };
 }
