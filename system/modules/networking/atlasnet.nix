44153f16411fa71b074075b36046d5eb6c27c9d8
Michal Atlas <michal_atlas+git@posteo.net>
2024-03-05 12:00:57 +0100

    refactor(atlasnet.nix-(wireguard)): Removed wireguard addresses

--- system/modules/networking/atlasnet.nix
+++ system/modules/networking/atlasnet.nix
@@ -1,11 +1,6 @@
-{ lib, ... }:
-let
-  prefix = "fd4c:16e4:7d9b:0";
-  wgip = idx: "${prefix}::${builtins.toString idx}/128";
-in
-{
+{lib, ...}: {
   options = {
-    atlasnet = lib.mkOption { };
+    atlasnet = lib.mkOption {};
   };
   config = {
     atlasnet = {
@@ -13,16 +8,6 @@ in
         "dagon" = "200:2b5a:7e80:7b31:7d15:6c81:2563:62c";
         "hydra" = "200:6229:6335:8721:7ae1:6b30:961e:c172";
         "lana" = "200:29bd:a495:4ad7:f79e:e29a:181a:3872";
-      };
-      wireguard = {
-        "hydra" = {
-          address = (wgip 1);
-          key = "0MigA4ewwzbwrlrZsi7+xhxn893q3nbtTPn6uiB2LEE=";
-        };
-        "dagon" = {
-          address = (wgip 2);
-          key = "VUk71x+wmwt//38RNT47ZNFJP0ZB2xB++4bAAtT6uEU=";
-        };
       };
     };
   };
