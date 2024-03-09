6efe6e2a5d716d3fb8dffe4ffdfb3c4d518f4ebc
Michal Atlas <michal_atlas+git@posteo.net>
2024-03-09 14:47:38 +0100

    style(btrfs): Format

--- lib/default.nix
+++ lib/default.nix
@@ -1,9 +1,11 @@
 {
-  btrfsMount =
-    dev: subvol: {
-      device = dev;
-      options = [ "subvol=${subvol}" "noatime"
-                  "compress=zstd" ];
-      fsType = "btrfs";
-    };
+  btrfsMount = dev: subvol: {
+    device = dev;
+    options = [
+      "subvol=${subvol}"
+      "noatime"
+      "compress=zstd"
+    ];
+    fsType = "btrfs";
+  };
 }
