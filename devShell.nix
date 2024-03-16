2e0358e06bf998194358bc3ca63c4fc9aad379c9
Michal Atlas <michal_atlas+git@posteo.net>
2024-03-16 16:07:42 +0100

    feat(eduroam): Add eduroam setup

--- devShell.nix
+++ devShell.nix
@@ -22,6 +22,15 @@
           build "$@" && ${pkgs.nix-diff}/bin/nix-diff /run/current-system result && rm result
         '')
         pkgs.commitizen
+        (pkgs.writeShellScriptBin "bootstrap-eduroam" ''
+          ${pkgs.python3.withPackages (py: [
+            py.dbus-python
+          ])}/bin/python ${builtins.fetchurl {
+            name = "cateduroam.py";
+            url = "https://cat.eduroam.org/user/API.php?action=downloadInstaller&lang=en&profile=2904&device=linux&generatedfor=user&openroaming=0";
+            sha256 = "sha256:0nrpqal4lxfs264r68fx9285vv4wkzc0r4ipza7sizazvawxm4i9";
+          }}
+        '')
       ];
       inherit (self.checks.${system}.pre-commit-check) shellHook;
     };
