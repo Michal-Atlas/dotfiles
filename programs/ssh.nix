caa962561a2891914786eb68de3686137b95ee9d
Michal Atlas <michal_atlas+git@posteo.net>
2024-04-22 13:19:07 +0200

    feat(ssh): Add fray

--- programs/ssh.nix
+++ programs/ssh.nix
@@ -7,6 +7,14 @@ _: {
         hostname = "the-dam.org";
         identityFile = "/home/michal_atlas/.ssh/the-dam";
       };
+      fray1 = {
+        user = "zacekmi2";
+        hostname = "fray1.fit.cvut.cz";
+        extraOptions = {
+          ControlMaster = "auto";
+          HostKeyAlgorithms = "+ssh-rsa";
+        };
+      };
     };
   };
 }
