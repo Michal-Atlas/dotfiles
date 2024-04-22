462ba53ad6211fb260b8c6e06ad19bd52a8cd6e5
Michal Atlas <michal_atlas+git@posteo.net>
2024-04-22 15:51:51 +0200

    fix(hydra): Remove router mount

--- machines/hydra/filesystems.nix
+++ machines/hydra/filesystems.nix
@@ -10,21 +10,6 @@ with lib; {
       "/bignix" = "rpool/bignix";
     }
     // {
-      "/RouterDisk" = {
-        device = "//192.168.0.1/sda1";
-        fsType = "cifs";
-        options = let
-          # https://wiki.nixos.org/wiki/Samba#Samba_Client
-          automount_opts = [
-            "x-systemd.automount"
-            "noauto"
-            "x-systemd.idle-timeout=60"
-            "x-systemd.device-timeout=5s"
-            "x-systemd.mount-timeout=5s"
-          ];
-        in
-          automount_opts ++ ["vers=1.0" "user=anonymous" "pass=no"];
-      };
       "/boot/efi" = {
         device = "/dev/disk/by-uuid/C9ED-A99E";
         fsType = "vfat";
