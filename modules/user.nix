d21019d754af2a210e0f4b4c55d148a490fdfafb
Michal Atlas <michal_atlas+git@posteo.net>
2024-06-13 19:33:30 +0200

    feat(users): Add david

--- modules/user.nix
+++ modules/user.nix
@@ -19,6 +19,14 @@
       openssh.authorizedKeys.keys = with builtins; (map (f: readFile ../keys/${f}) (attrNames (readDir ../keys)));
       linger = true;
     };
+    users.david = {
+      isNormalUser = true;
+      shell = pkgs.bashInteractive;
+      openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICdenk+u4qOiOnmZfN/Ro8V00B0C8tQOEFacRLna7qn9 dav@David3En"
+      ];
+    };
+    
     mutableUsers = false;
     users.root.hashedPassword = "$y$j9T$BJgm2ampHrpbLgQhzXNw4.$xppBStrecndUWp4AHAdAt3vZ7.XHmuXvNTL3WgJ0NyC";
   };
