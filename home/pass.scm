826f071a7002467b527d98d239790bd5648efd5a
Michal Atlas <michal_atlas+git@posteo.net>
2024-06-13 19:52:12 +0200

    refactor: Remove Guix

--- home/pass.scm
+++
@@ -1,8 +0,0 @@
-(define-module (home pass)
-  #:use-module (gnu home-services password-utils)
-  #:use-module (atlas utils services)
-  #:export (pass))
-
-(define pass
-  (&s home-password-store
-      (browserpass-native? #t)))
