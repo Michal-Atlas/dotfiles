2ec7fc5695efb2f5490f5b83541488673fee2208
Michal Atlas <michal_atlas+git@posteo.net>
2024-06-16 16:14:27 +0200

    feat(fonts): Add defaults

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,27 +1,23 @@
 {pkgs, ...}: {
-  fonts.packages = with pkgs; [
-    (nerdfonts.override {
-      fonts = [
-        "Hack"
-        "IBMPlexMono"
-        "Iosevka"
-        "Terminus"
-        "NerdFontsSymbolsOnly"
-      ];
-    })
-    font-awesome
-    source-code-pro
-    fira-code
-    fira-mono
-    dejavu_fonts
-    dina-font
-    fira-code-symbols
-    jetbrains-mono
-    liberation_ttf
-    mplus-outline-fonts.githubRelease
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    source-han-sans
-  ];
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      (nerdfonts.override {
+        fonts = [
+          "Hack"
+          "IBMPlexMono"
+          "Iosevka"
+          "Terminus"
+          "NerdFontsSymbolsOnly"
+        ];
+      })
+      font-awesome
+      fira-code
+      fira-mono
+      fira-code-symbols
+      jetbrains-mono
+      mplus-outline-fonts.githubRelease
+      noto-fonts
+    ];
+  };
 }
