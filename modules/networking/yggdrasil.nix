acd14b4140c0fc0e5fae8f133e90d553d15f688d
Michal Atlas <michal_atlas+git@posteo.net>
2024-06-16 10:28:55 +0200

    fix(yggdrasil): Actually allow peering

--- modules/networking/yggdrasil.nix
+++ modules/networking/yggdrasil.nix
@@ -1,4 +1,6 @@
-{config, ...}: {
+{config, ...}: let
+  multiCastPort = 123;
+in {
   networking.hosts = with builtins;
     listToAttrs (
       map
@@ -14,12 +16,27 @@
     openMulticastPort = true;
     configFile = config.age.secrets.yggdrasil.path;
 
-    settings.Peers = [
-      # Czechia
-      "tls://[2a03:3b40:fe:ab::1]:993"
-      "tls://37.205.14.171:993"
-      # Germany
-      "tcp://193.107.20.230:7743"
-    ];
+    settings = {
+      Peers = [
+        # Czechia
+        "tls://[2a03:3b40:fe:ab::1]:993"
+        "tls://37.205.14.171:993"
+        # Germany
+        "tcp://193.107.20.230:7743"
+      ];
+      MulticastInterfaces = [
+        {
+          Regex = ".*";
+          Beacon = true;
+          Listen = true;
+          Port = multiCastPort;
+          Priority = 0;
+          Password = "";
+        }
+      ];
+    };
   };
+  networking.firewall.allowedTCPPorts = [
+    multiCastPort
+  ];
 }
