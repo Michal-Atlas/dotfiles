6b6d6f067480289d7f5a02cd288b23d2b0a19469
Michal Atlas <michal_atlas+git@posteo.net>
2024-07-13 22:55:51 +0200

    feat(secrets): Update leviathan keys

--- secrets.nix
+++ secrets.nix
@@ -5,7 +5,7 @@ let
   hydra_sys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHp5iggvaDE3rbzNRteh6ckWVKicSFbTYMp1SeQYnRQ2 root@hydra";
   hydra_user = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDKTYuh5bYyuSHU1FlW//z1oW1fd64CNQV+Rxq4nPZdtf5+qUgP2IujebxdxDMOubYcBAKweDNpp2V4aDd7Y9OH7UaeuH3ZWs5qbLUqHurOdt30e1tcUZWMqvGQ1BLlm29T+XIdn37aaBv+sMcfMP7ziNIMl8CujM1+apN35qjrBEZHs0Eyue5ymt7C7ClcpC2iHJVk68xkdfNsGOhUkuD5o6KiQF9yFWOU21XqoQqBUYkburFWBEPTGxWtKQItz7nXW/+RbmEhmrT/FuVd5+bzK1rR52Fv5MEbfiV92OntoY4T7gigDR0JBC30aXK2BXBp3yBeaEBGodiSa52/y1kDH2qqRqs1Kz7oP2X34INi6rA5muhDGn2Qg9uwu5Qt1jUkLx+cOv7vdVmvcOFCpgYgdjnC0ETEsEyrDlZumWIAavEZniMw283KkaSYCMdDJlwOcSyVaY2LU4MuPWHUhlx4ot3Hh/m8UGNWwv4zcLr5DxQh6O8d9UIc3oCOgVv0ozM= michal_atlas@hydra";
   hydra = [hydra_user hydra_sys];
-  leviathan_sys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBOeEw7ljLsBf34zFdBaOfYRisDTATKednG7l4o/aZm+ root@leviathan";
+  leviathan_sys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINxxlUcmJvrFYOyzWovlFVGN3ovl57mVDE5XSAMSpkf8 root@leviathan";
   leviathan = [leviathan_sys dagon_user];
 in {
   "secrets/yggdrasil/dagon.json".publicKeys = dagon;
