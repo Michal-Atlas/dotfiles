e0b459dd90a508210a43016175e74922f6d6e0cf
Michal Atlas <michal_atlas+git@posteo.net>
2024-07-27 15:12:30 +0200

    fix(pano): Update to 23 alpha

--- packages.nix
+++ packages.nix
@@ -141,7 +141,15 @@ with pkgs;
   ++ (with gnomeExtensions; [
     appindicator
     mpris-label
-    pano
+    (pano.overrideAttrs {
+      version = "23-alpha2";
+      src = fetchzip {
+        name = "pano-src";
+        url = "https://github.com/oae/gnome-shell-pano/releases/download/v23-alpha2/pano@elhan.io.zip";
+        sha256 = "sha256-Y8WgVUHX094RUwYKdt7OROPZMl3dakK0zOU9OTdyqxc=";
+        stripRoot = false;
+      };
+    })
   ])
   ++ [
     (pkgs.python311.withPackages
