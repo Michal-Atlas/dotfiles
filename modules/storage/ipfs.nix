ada5fcb972c2e8e2867df2ba7fc3c3eb734e684c
Michal Atlas <michal_atlas+git@posteo.net>
2024-08-08 00:15:03 +0200

    feat(ipfs): Give up on NATing, publish Yggdrasil

--- modules/storage/ipfs.nix
+++ modules/storage/ipfs.nix
@@ -1,9 +1,19 @@
-_: {
+{config, ...}: {
   services.kubo = {
     enable = true;
     localDiscovery = true;
     autoMount = true;
-    settings.Addresses.API = ["/ip4/127.0.0.1/tcp/5001"];
+    settings = {
+      Addresses = {
+        API = ["/ip4/127.0.0.1/tcp/5001"];
+        AppendAnnounce = [
+          "/ip6/${builtins.getAttr
+            config.networking.hostName
+            config.atlasnet.yggdrasil}/tcp/4001"
+        ];
+      };
+      AutoNAT.ServiceMode = "disabled";
+    };
   };
   networking.firewall = {
     allowedTCPPorts = [4001];
