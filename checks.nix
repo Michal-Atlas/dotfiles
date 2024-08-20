5f66aea94cebbc051af57583cb07db7b5b5ec757
Michal Atlas <michal_atlas+git@posteo.net>
2024-08-20 18:34:40 +0200

    feat(flake): Format to nixfmt instead of alejandra

--- checks.nix
+++ checks.nix
@@ -1,9 +1,9 @@
 _: {
-  perSystem = {pkgs, ...}: {
+  perSystem = { pkgs, ... }: {
     pre-commit = {
       check.enable = true;
       settings.hooks = {
-        alejandra.enable = true;
+        nixfmt.enable = true;
         shellcheck.enable = true;
         statix.enable = true;
         shfmt.enable = true;
@@ -15,12 +15,12 @@ _: {
           enable = true;
           name = "Elisp Autofmt";
           entry = "${
-            (pkgs.writeShellScriptBin "autofmt.sh" ''
-              ${
-                (pkgs.emacsPackagesFor pkgs.emacs).emacsWithPackages
-                (epkgs: with epkgs; [elisp-autofmt])
-              }/bin/emacs "$1" --batch --eval "(require 'elisp-autofmt)" -f elisp-autofmt-buffer -f save-buffer --kill'')
-          }/bin/autofmt.sh";
+              (pkgs.writeShellScriptBin "autofmt.sh" ''
+                ${
+                  (pkgs.emacsPackagesFor pkgs.emacs).emacsWithPackages
+                  (epkgs: with epkgs; [ elisp-autofmt ])
+                }/bin/emacs "$1" --batch --eval "(require 'elisp-autofmt)" -f elisp-autofmt-buffer -f save-buffer --kill'')
+            }/bin/autofmt.sh";
           files = "\\.(el)$";
         };
       };
