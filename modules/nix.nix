e4c9ec5350e18f17896fec1ca04617323f140373
Michal Atlas <michal_atlas+git@posteo.net>
2024-08-20 20:28:24 +0200

    feat(nix): Automate long-term GC

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,22 +1,26 @@
-{
-  emacs-overlay,
-  unison-nix,
-  ...
-}: {
+{ flake, ... }:
+let GtoBString = g: builtins.toString (g * 1024 * 1024 * 1024);
+in {
   nix = {
     settings = {
-      trusted-users = ["root" "@wheel"];
-      experimental-features = ["nix-command" "flakes"];
-      auto-optimise-store = true;
+      trusted-users = [ "root" "@wheel" ];
+      experimental-features = [ "nix-command" "flakes" ];
+      min-free = GtoBString 30;
+      max-free = GtoBString 50;
+      min-free-check-interval = 60 * 60;
+    };
+    optimise.automatic = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d --max-freed 0";
     };
   };
   nixpkgs = {
-    overlays = [
+    overlays = with flake.inputs; [
       emacs-overlay.overlays.default
       unison-nix.overlay
     ];
-    config = {
-      allowUnfree = true;
-    };
+    config = { allowUnfree = true; };
   };
 }
