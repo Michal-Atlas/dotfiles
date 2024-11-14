50f01f62d0c3a11e2c78d706edf4f97df87a7bbc
Michal Atlas <michal_atlas+git@posteo.net>
2024-11-15 00:15:32 +0100

    Enable lsp-ui automatic showing

--- home/dot-config/doom/config.el
+++ home/dot-config/doom/config.el
@@ -74,3 +74,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+(setq lsp-ui-doc-position 'top
+      lsp-ui-doc-delay 0.2
+      lsp-ui-doc-show-with-cursor t)
