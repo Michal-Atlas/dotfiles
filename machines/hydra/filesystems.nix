53c8dd22e94be15a1cdd553739e00f0b56f192bc
Michal Atlas <michal_atlas+git@posteo.net>
2024-12-03 15:35:57 +0100

    feat(hydra): Filesystems

--- machines/hydra/filesystems.nix
+++ machines/hydra/filesystems.nix
@@ -4,16 +4,13 @@ with flake.self.lib;
   swapDevices = [ { device = "/dev/disk/by-uuid/1a20f616-635b-46af-bf07-ff09cf461504"; } ];
 
   fileSystems =
-    zfsMounts {
-      "/home/michal_atlas/Games" = "rpool/games";
-      "/home/michal_atlas" = "rpool/home/michal_atlas";
-      "/var" = "rpool/var";
-    }
-    // {
+
+    {
+      "/home/michal_atlas" = btrfsMount "/dev/disk/by-uuid/880d14ef-f964-4956-84a7-e457db80a5ad" "@home";
       "/boot/efi" = {
         device = "/dev/disk/by-uuid/C9ED-A99E";
         fsType = "vfat";
       };
-      "/" = btrfsMount "/dev/rpool/root" "@nix";
+      "/" = btrfsMount "/dev/disk/by-uuid/65920de2-b793-4480-bda7-4e04c4d9eb59" "@nix";
     };
 }
