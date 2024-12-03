a93602079b888162119675382a6c73a8071d6b7f
Michal Atlas <michal_atlas+git@posteo.net>
2024-12-03 18:37:21 +0100

    feat(gnome): Use up-to-date pano

--- home/gnome.nix
+++ home/gnome.nix
@@ -7,15 +7,7 @@
       builtins.map (package: { inherit package; }) [
         appindicator
         mpris-label
-        (pano.overrideAttrs {
-          version = "23-alpha2";
-          src = pkgs.fetchzip {
-            name = "pano-src";
-            url = "https://github.com/oae/gnome-shell-pano/releases/download/v23-alpha2/pano@elhan.io.zip";
-            sha256 = "sha256-Y8WgVUHX094RUwYKdt7OROPZMl3dakK0zOU9OTdyqxc=";
-            stripRoot = false;
-          };
-        })
+        pano
         removable-drive-menu
         resource-monitor
         caffeine
