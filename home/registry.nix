9002a550be983cbe91c87222b2292752b3df1098
Michal Atlas <michal_atlas+git@posteo.net>
2024-12-05 00:18:49 +0100

    fix(flake): Remove self from registry, FOR NOW

--- home/registry.nix
+++ home/registry.nix
@@ -2,5 +2,5 @@
 {
   nix.registry = {
     np.to = builtins.parseFlakeRef "github:numtide/nixpkgs-unfree/nixos-unstable";
-  } // lib.mapAttrs (_: value: { flake = value; }) flake.inputs;
+  } // lib.mapAttrs (_: value: { flake = value; }) (builtins.removeAttrs flake.inputs [ "self" ]);
 }
