7c94ee1bb208b681033adfef9627187d1b0ed7a8
Michal Atlas <me+git@michal-atlas.cz>
2024-12-05 23:17:06 +0100

    feat(atlasnet): Start building new scheme

--- modules/networking/hosts.nix
+++ modules/networking/hosts.nix
@@ -1,4 +1,11 @@
 _: {
+  networking.hosts = {
+    "37.205.15.189" = [ "vorpal" ];
+    "2a03:3b40:fe:833::1" = [ "vorpal" ];
+    "200:299c:6b1a:c8ac:bc4d:d2be:cbd:6014" = [ "leviathan" ];
+    "200:8c2a:3279:eb5d:f3eb:eb7f:d3a3:29d4" = [ "hydra" ];
+  };
+
   networking.stevenBlackHosts = {
     enable = true;
     blockFakenews = true;
--- modules/networking/yggdrasil.nix
+++ modules/networking/yggdrasil.nix
@@ -11,11 +11,7 @@ in
 
     settings = {
       Peers = [
-        # Czechia
-        "tls://[2a03:3b40:fe:ab::1]:993"
-        "tls://37.205.14.171:993"
-        # Germany
-        "tcp://193.107.20.230:7743"
+        "tls://vorpal:993"
       ];
       MulticastInterfaces = [
         {
