1bd08b9d9b666df64eefd2318b0f75fa26ad9e21
Michal Atlas <me+git@michal-atlas.cz>
2024-12-08 21:49:18 +0100

    fix(check): Use emacs-nox

--- checks.nix
+++ checks.nix
@@ -1,30 +1,26 @@
 _: {
-  perSystem = { pkgs, ... }: {
-    pre-commit = {
-      check.enable = true;
-      settings.hooks = {
-        nixfmt-rfc-style.enable = true;
-        shellcheck.enable = true;
-        statix.enable = true;
-        shfmt.enable = true;
-        nil.enable = true;
-        check-yaml.enable = true;
-        deadnix.enable = true;
-        commitizen.enable = true;
+  perSystem =
+    { pkgs, ... }:
+    {
+      pre-commit = {
+        check.enable = true;
+        settings.hooks = {
+          nixfmt-rfc-style.enable = true;
+          shellcheck.enable = true;
+          statix.enable = true;
+          shfmt.enable = true;
+          nil.enable = true;
+          check-yaml.enable = true;
+          deadnix.enable = true;
+          commitizen.enable = true;
 
-        elisp-autofmt = {
-          enable = true;
-          name = "Elisp Autofmt";
-          entry = "${
-              (pkgs.writeShellScriptBin "autofmt.sh" ''
-                ${
-                  (pkgs.emacsPackagesFor pkgs.emacs).emacsWithPackages
-                  (epkgs: with epkgs; [ elisp-autofmt ])
-                }/bin/emacs "$1" --batch --eval "(require 'elisp-autofmt)" -f elisp-autofmt-buffer -f save-buffer --kill'')
-            }/bin/autofmt.sh";
-          files = "\\.(el)$";
+          elisp-autofmt = {
+            enable = true;
+            name = "Elisp Autofmt";
+            entry = "${(pkgs.writeShellScriptBin "autofmt.sh" ''${(pkgs.emacsPackagesFor pkgs.emacs-nox).emacsWithPackages (epkgs: with epkgs; [ elisp-autofmt ])}/bin/emacs "$1" --batch --eval "(require 'elisp-autofmt)" -f elisp-autofmt-buffer -f save-buffer --kill'')}/bin/autofmt.sh";
+            files = "\\.(el)$";
+          };
         };
       };
     };
-  };
 }
