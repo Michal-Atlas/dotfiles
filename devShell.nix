dcdf9201f15225b086e8392b467b1e67a502cfd1
Michal Atlas <me+git@michal-atlas.cz>
2024-12-17 01:04:28 +0100

    feat(flake): Add set -x in devShell scripts

--- devShell.nix
+++ devShell.nix
@@ -12,7 +12,10 @@
         shellHook = config.pre-commit.installationScript;
         nativeBuildInputs =
           let
-            rebuild-cmd = cmd: ''${pkgs.nh}/bin/nh os ${cmd} . "$@";'';
+            prefix = "set -euox pipefail";
+            rebuild-cmd = cmd: ''
+              ${prefix}
+              ${pkgs.nh}/bin/nh os ${cmd} . "$@";'';
           in
           [
             pkgs.nh
@@ -22,7 +25,12 @@
             (pkgs.writeShellScriptBin "build" (rebuild-cmd "build"))
             (pkgs.writeShellScriptBin "check" ''nix flake check . "$@"'')
             (pkgs.writeShellScriptBin "recdiff" ''
+              ${prefix}
               nixos-rebuild build --flake . "$@" && ${pkgs.nix-diff}/bin/nix-diff /run/current-system result --character-oriented --context 10 && rm result
+            '')
+            (pkgs.writeShellScriptBin "deploy" ''
+              ${prefix}
+              nixos-rebuild switch --flake .#"$1" --target-host "$1" --use-remote-sudo --use-substitutes
             '')
             inputs.agenix.packages.${system}.default
             pkgs.commitizen
