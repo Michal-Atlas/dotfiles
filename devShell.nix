243b911697d47a259292a11dfb490c41e6aff1b9
Michal Atlas <me+git@michal-atlas.cz>
2024-12-17 01:04:55 +0100

    feat(pkgs): Move bootstrap-eduroam to permanent packages

--- devShell.nix
+++ devShell.nix
@@ -34,15 +34,6 @@
             '')
             inputs.agenix.packages.${system}.default
             pkgs.commitizen
-            (pkgs.writeShellScriptBin "bootstrap-eduroam" ''
-              ${pkgs.python3.withPackages (py: [ py.dbus-python ])}/bin/python ${
-                builtins.fetchurl {
-                  name = "cateduroam.py";
-                  url = "https://cat.eduroam.org/user/API.php?action=downloadInstaller&lang=en&profile=2904&device=linux&generatedfor=user&openroaming=0";
-                  sha256 = "sha256:1lnbzbxfwlqzdrsrdkba5rnqwm6aijkw4xjn56rjqyyjib9h2295";
-                }
-              }
-            '')
           ];
       };
     };
--- packages.nix
+++ packages.nix
@@ -1,8 +1,17 @@
 pkgs:
 with pkgs;
 [
-  # keep-sorted start
+  (pkgs.writeShellScriptBin "bootstrap-eduroam" ''
+    ${pkgs.python3.withPackages (py: [ py.dbus-python ])}/bin/python ${
+      builtins.fetchurl {
+        name = "cateduroam.py";
+        url = "https://cat.eduroam.org/user/API.php?action=downloadInstaller&lang=en&profile=2904&device=linux&generatedfor=user&openroaming=0";
+        sha256 = "sha256:1lnbzbxfwlqzdrsrdkba5rnqwm6aijkw4xjn56rjqyyjib9h2295";
+      }
+    }
+  '')
   (hiPrio gcc)
+  # keep-sorted start
   acl
   alejandra
   ani-cli
