6e2f50c0127928a84aef256d14cafb71c9df749e
Michal Atlas <me+git@michal-atlas.cz>
2024-12-20 03:18:18 +0100

    feat(pixelfed): Add

--- machines/vpsFree/social.nix
+++ machines/vpsFree/social.nix
@@ -20,6 +20,19 @@
         extraConfig = ''
           proxy_set_header Host tube.${config.networking.domain};
         '';
+    pixelfed = {
+      enable = true;
+      domain = "pixel.${config.networking.domain}";
+      redis.createLocally = true;
+      database.createLocally = true;
+      settings.PF_OPTIMIZE_IMAGES = false;
+      secretFile = "/var/lib/pixelfed/secrets";
+      nginx = {
+        enableACME = true;
+        forceSSL = true;
+      };
+      maxUploadSize = "50M";
+    };
       };
     };
   };
