2dcd8b92656a06fd1d218536edb5f1944a99bd67
Michal Atlas <michal_atlas+git@posteo.net>
2024-12-28 21:39:47 +0100

    fix(vpsFree): Remove socials since they're on blog

--- machines/vpsFree/social.nix
+++ machines/vpsFree/social.nix
@@ -3,27 +3,7 @@
   ...
 }:
 {
-  networking.firewall.allowedTCPPorts = [
-    config.services.writefreely.settings.server.gopher_port
-  ];
   services = {
-    writefreely = {
-      enable = true;
-      admin.name = "root";
-      host = "text.${config.networking.domain}";
-      acme.enable = true;
-      nginx = {
-        enable = true;
-        forceSSL = true;
-      };
-      settings = {
-        server.gopher_port = 70;
-        app = {
-          single_user = true;
-          federation = true;
-        };
-      };
-    };
     mastodon = {
       #      enable = true;
       localDomain = "social.${config.networking.domain}";
@@ -31,31 +11,6 @@
       streamingProcesses = 2;
       smtp.fromAddress = "noreply@social.michal-atlas.cz";
       extraConfig.SINGLE_USER_MODE = "true";
-    };
-    pixelfed = {
-      enable = true;
-      domain = "pixel.${config.networking.domain}";
-      redis.createLocally = true;
-      database.createLocally = true;
-      settings.PF_OPTIMIZE_IMAGES = false;
-      secretFile = "/var/lib/pixelfed/secrets";
-      nginx = {
-        enableACME = true;
-        forceSSL = true;
-      };
-      maxUploadSize = "50M";
-    };
-    nginx.virtualHosts = {
-      "tube.michal-atlas.cz" = {
-        forceSSL = true;
-        enableACME = true;
-        locations."/" = {
-          proxyPass = "http://hydra:9000";
-          extraConfig = ''
-            client_max_body_size 2G;
-          '';
-        };
-      };
     };
   };
 }
