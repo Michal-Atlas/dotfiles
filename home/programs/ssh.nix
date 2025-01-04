19b25026202448009184007832ae08252ff30ab8
Michal Atlas <me+git@michal-atlas.cz>
2025-01-04 15:24:06 +0100

    fix(tmux): Don't open in all shells, bad idea

--- home/programs/ssh.nix
+++ home/programs/ssh.nix
@@ -1,20 +1,31 @@
 _: {
   programs.ssh = {
     enable = true;
-    matchBlocks = {
-      dam = {
-        user = "atlas";
-        hostname = "the-dam.org";
-        identityFile = "/home/michal_atlas/.ssh/the-dam";
-      };
-      fray1 = {
-        user = "zacekmi2";
-        hostname = "fray1.fit.cvut.cz";
-        extraOptions = {
-          ControlMaster = "auto";
-          HostKeyAlgorithms = "+ssh-rsa";
+    matchBlocks =
+      let
+        tmuxBlock = {
+          extraOptions = {
+            RequestTTY = "yes";
+            RemoteCommand = "tmux new-session -A -s ssh";
+          };
+        };
+      in
+      {
+        hydra = tmuxBlock;
+        leviathan = tmuxBlock;
+        dam = {
+          user = "atlas";
+          hostname = "the-dam.org";
+          identityFile = "/home/michal_atlas/.ssh/the-dam";
+        };
+        fray1 = {
+          user = "zacekmi2";
+          hostname = "fray1.fit.cvut.cz";
+          extraOptions = {
+            ControlMaster = "auto";
+            HostKeyAlgorithms = "+ssh-rsa";
+          };
         };
       };
-    };
   };
 }
--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -22,8 +22,6 @@
       MOZ_USE_XINPUT2 = "1";
       GRIM_DEFAULT_DIR = "~/tmp";
       _JAVA_AWT_WM_NONREPARENTING = "1";
-      ZSH_TMUX_UNICODE = true;
-      ZSH_TMUX_AUTOSTART = true;
     };
     initExtra = ''
       setopt interactivecomments
@@ -34,16 +32,6 @@
         name = "fzf-tab";
         file = "share/fzf-tab/fzf-tab.plugin.zsh";
         src = zsh-fzf-tab;
-      }
-      {
-        name = "tmux-plugin-omz";
-        file = "plugins/tmux/tmux.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "ohmyzsh";
-          repo = "ohmyzsh";
-          rev = "f733dc340b2a1c5b2e61a4da7de790b2f557175f";
-          hash = "sha256-67DSpgD5v9RIahW3SYhL9AtOToYfKMt2xrCFZSh3arI";
-        };
       }
     ];
   };
