b0e347782bd8d839f1ae2ae00e5305905a18d035
Michal Atlas <me+git@michal-atlas.cz>
2025-01-04 15:26:51 +0100

    feat(bees): Add on hydra

--- machines/hydra/filesystems.nix
+++ machines/hydra/filesystems.nix
@@ -1,20 +1,28 @@
 { flake, ... }:
 with flake.self.lib;
+let
+  DISKA_UUID = "880d14ef-f964-4956-84a7-e457db80a5ad";
+  DISKB_UUID = "65920de2-b793-4480-bda7-4e04c4d9eb59";
+in
 {
   swapDevices = [ { device = "/dev/disk/by-uuid/1a20f616-635b-46af-bf07-ff09cf461504"; } ];
 
   fileSystems =
 
     {
-      "/home/michal_atlas" = btrfsMount "/dev/disk/by-uuid/880d14ef-f964-4956-84a7-e457db80a5ad" "@home";
+      "/home/michal_atlas" = btrfsMount "/dev/disk/by-uuid/${DISKA_UUID}" "@home";
       "/boot/efi" = {
         device = "/dev/disk/by-uuid/C9ED-A99E";
         fsType = "vfat";
       };
-      "/" = btrfsMount "/dev/disk/by-uuid/65920de2-b793-4480-bda7-4e04c4d9eb59" "@nix";
-      "/DISKA" = btrfsMount "/dev/disk/by-uuid/880d14ef-f964-4956-84a7-e457db80a5ad" "/";
-      "/DISKB" = btrfsMount "/dev/disk/by-uuid/65920de2-b793-4480-bda7-4e04c4d9eb59" "/";
+      "/" = btrfsMount "/dev/disk/by-uuid/${DISKB_UUID}" "@nix";
+      "/DISKA" = btrfsMount "/dev/disk/by-uuid/${DISKA_UUID}" "/";
+      "/DISKB" = btrfsMount "/dev/disk/by-uuid/${DISKB_UUID}" "/";
     };
+  services.beesd.filesystems = {
+    "DISKB".spec = "UUID=${DISKB_UUID}";
+    "DISKA".spec = "UUID=${DISKA_UUID}";
+  };
   services.btrbk.instances."main".settings = {
     timestamp_format = "long-iso";
     snapshot_preserve_min = "1w";
