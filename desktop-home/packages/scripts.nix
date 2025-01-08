0b72afe809c039a61ed2a1b2932095057eb10e19
Michal Atlas <me+git@michal-atlas.cz>
2025-01-08 18:17:25 +0100

    fix(bootstrap-eduroam): Script hash changed again

--- desktop-home/packages/scripts.nix
+++ desktop-home/packages/scripts.nix
@@ -6,7 +6,7 @@
         builtins.fetchurl {
           name = "cateduroam.py";
           url = "https://cat.eduroam.org/user/API.php?action=downloadInstaller&lang=en&profile=2904&device=linux&generatedfor=user&openroaming=0";
-          sha256 = "sha256:1lnbzbxfwlqzdrsrdkba5rnqwm6aijkw4xjn56rjqyyjib9h2295";
+          sha256 = "sha256:00cn00c6b6q2wclz4r0x3z9bas6860bjygv0kgpdw3amrzzxsc09";
         }
       }
     '')
