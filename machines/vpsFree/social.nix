048b336b6a35c7952dd661928480f17d5c35aed1
Michal Atlas <michal_atlas+git@posteo.net>
2025-01-29 02:58:13 +0100

    feat(vpsFree/mastodon): Remove

--- machines/vpsFree/social.nix
+++ machines/vpsFree/social.nix
@@ -5,14 +5,6 @@
 }:
 {
   services = {
-    mastodon = {
-      enable = true;
-      localDomain = "social2.${config.networking.domain}";
-      configureNginx = true;
-      streamingProcesses = 2;
-      smtp.fromAddress = "noreply@social.michal-atlas.cz";
-      extraConfig.SINGLE_USER_MODE = "true";
-    };
     gotosocial = {
       enable = true;
       settings = {
