b2aedfb6a28424e8454bd13e77966bec3781c972
Michal Atlas <me+git@michal-atlas.cz>
2025-02-11 00:12:29 +0100

    feat(hosts): Add vorpal yggdrasil address

--- modules/networking/hosts.nix
+++ modules/networking/hosts.nix
@@ -8,8 +8,10 @@ _: {
       "vorpal"
       "vorpal.ip6"
     ];
+
     "200:299c:6b1a:c8ac:bc4d:d2be:cbd:6014" = [ "leviathan" ];
     "201:876c:b6b:3561:f064:c3d6:5135:36e6" = [ "hydra" ];
+    "201:df2c:b49a:7f3d:d54d:5d26:18c9:fc54" = [ "yg.vorpal" ];
   };
 
   networking.stevenblack.enable = true;
