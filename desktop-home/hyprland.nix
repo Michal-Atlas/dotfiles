6ae3d90f764a85e5fb3c60d1a752f14acbeda281
Michal Atlas <me+git@michal-atlas.cz>
2025-02-11 13:57:08 +0100

    feat(hyprland): Remove locking for now

--- desktop-home/hyprland.nix
+++ desktop-home/hyprland.nix
@@ -39,10 +39,6 @@ in
       };
       listener = [
         {
-          timeout = 900;
-          on-timeout = "hyprlock";
-        }
-        {
           timeout = 1200;
           on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
