4091257122b1b0f25853997451d81e55cae2e145
Michal Atlas <me+git@michal-atlas.cz>
2025-02-11 15:17:42 +0100

    feat(stevenblackhosts): Block all

--- modules/networking/hosts.nix
+++ modules/networking/hosts.nix
@@ -1,18 +1,27 @@
 _: {
-  networking.hosts = {
-    "37.205.15.189" = [
-      "vorpal"
-      "vorpal.ip4"
-    ];
-    "2a03:3b40:fe:833::1" = [
-      "vorpal"
-      "vorpal.ip6"
-    ];
+  networking = {
+    hosts = {
+      "37.205.15.189" = [
+        "vorpal"
+        "vorpal.ip4"
+      ];
+      "2a03:3b40:fe:833::1" = [
+        "vorpal"
+        "vorpal.ip6"
+      ];
 
-    "200:299c:6b1a:c8ac:bc4d:d2be:cbd:6014" = [ "leviathan" ];
-    "201:876c:b6b:3561:f064:c3d6:5135:36e6" = [ "hydra" ];
-    "201:df2c:b49a:7f3d:d54d:5d26:18c9:fc54" = [ "yg.vorpal" ];
+      "200:299c:6b1a:c8ac:bc4d:d2be:cbd:6014" = [ "leviathan" ];
+      "201:876c:b6b:3561:f064:c3d6:5135:36e6" = [ "hydra" ];
+      "201:df2c:b49a:7f3d:d54d:5d26:18c9:fc54" = [ "yg.vorpal" ];
+    };
+    stevenblack = {
+      enable = true;
+      block = [
+        "fakenews"
+        "gambling"
+        "porn"
+        "social"
+      ];
+    };
   };
-
-  networking.stevenblack.enable = true;
 }
