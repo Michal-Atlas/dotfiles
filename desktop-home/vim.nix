2b49f87cfbb679ed631f4998ddd48b502ccc71c2
Michal Atlas <michal_atlas+git@posteo.net>
2025-02-16 18:48:16 +0100

    feat(vim): Move plugins to desktop

--- desktop-home/vim.nix
+++ desktop-home/vim.nix
@@ -1,6 +1,26 @@
 _: {
   programs.nixvim.plugins = {
+    # keep-sorted start
+    auto-save.enable = true;
+    cursorline.enable = true;
+    direnv.enable = true;
+    haskell-scope-highlighting.enable = true;
+    lightline.enable = true;
+    lsp-signature.enable = true;
+    markdown-preview.enable = true;
+    neogit.enable = true;
+    nix.enable = true;
+    nvim-autopairs.enable = true;
+    project-nvim.enable = true;
+    rustaceanvim.enable = true;
+    telescope.enable = true;
+    tmux-navigator.enable = true;
+    treesitter.enable = true;
+    vim-slime.enable = true;
+    vimtex.enable = true;
     web-devicons.enable = true;
+    which-key.enable = true;
+    # keep-sorted end
     lsp = {
       enable = true;
       inlayHints = true;
--- home/vim.nix
+++ home/vim.nix
@@ -11,28 +11,6 @@ _: {
         options.desc = "Format given file with Nix";
       }
     ];
-    plugins = {
-      # keep-sorted start
-      auto-save.enable = true;
-      cursorline.enable = true;
-      direnv.enable = true;
-      haskell-scope-highlighting.enable = true;
-      lightline.enable = true;
-      lsp-signature.enable = true;
-      markdown-preview.enable = true;
-      neogit.enable = true;
-      nix.enable = true;
-      nvim-autopairs.enable = true;
-      project-nvim.enable = true;
-      rustaceanvim.enable = true;
-      telescope.enable = true;
-      tmux-navigator.enable = true;
-      treesitter.enable = true;
-      vim-slime.enable = true;
-      vimtex.enable = true;
-      which-key.enable = true;
-      # keep-sorted end
-    };
     opts = {
       number = true;
       relativenumber = true;
