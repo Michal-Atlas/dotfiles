bb5e0bbc55c9977c8a8ec600c07e731725739f0f
Michal Atlas <me+git@michal-atlas.cz>
2025-02-19 20:28:27 +0100

    feat(hosts): Disable StevenBlack

--- modules/networking/hosts.nix
+++ modules/networking/hosts.nix
@@ -14,13 +14,5 @@ _: {
       "201:876c:b6b:3561:f064:c3d6:5135:36e6" = [ "hydra" ];
       "201:df2c:b49a:7f3d:d54d:5d26:18c9:fc54" = [ "yg.vorpal" ];
     };
-    stevenblack = {
-      enable = true;
-      block = [
-        "fakenews"
-        "gambling"
-        "porn"
-      ];
-    };
   };
 }
