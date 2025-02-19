1cffd24b0f64915e76447bafda4f5a81dc20c0eb
Michal Atlas <me+git@michal-atlas.cz>
2025-02-19 20:59:18 +0100

    fix(nginx): Remove old setup

--- desktop-modules/default.nix
+++ desktop-modules/default.nix
@@ -53,21 +53,6 @@
       settings.General.Experimental = true;
     };
   };
-  services.nginx = {
-    enable = true;
-    virtualHosts."127.0.0.1" = {
-      listen = [
-        {
-          port = 8091;
-          addr = "127.0.0.1";
-        }
-      ];
-      locations = {
-        "/".proxyPass = "http://127.0.0.1:3000";
-        "/api".proxyPass = "http://127.0.0.1:8090";
-      };
-    };
-  };
   programs = {
     spicetify =
       let
