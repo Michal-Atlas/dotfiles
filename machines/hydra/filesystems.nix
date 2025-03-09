442d3a236b0b07646de164a7a2b9fa51faea6864
Michal Atlas <michal_atlas+git@posteo.net>
2025-03-09 02:59:49 +0100

    feat(hydra/fileSystems): Finalize root, remove BTRFS

--- machines/hydra/filesystems.nix
+++ machines/hydra/filesystems.nix
@@ -1,15 +1,8 @@
 { flake, ... }:
 with flake.self.lib;
-let
-  DISKN_UUID = "00061528-79fc-4dce-8e9e-3d78b23cd7a6";
-in
 {
   swapDevices = [ { device = "/dev/disk/by-uuid/1a20f616-635b-46af-bf07-ff09cf461504"; } ];
   boot.zfs.extraPools = [ "blackpool" ];
-  systemd.tmpfiles.rules = [
-    "e /DISKN/@cache/cache - - - 60d"
-  ];
-
   fileSystems =
 
     {
@@ -21,9 +14,10 @@ in
         device = "/dev/disk/by-uuid/C9ED-A99E";
         fsType = "vfat";
       };
-      "/" = btrfsMount "/dev/disk/by-uuid/${DISKN_UUID}" "@nix";
-      "/DISKN" = btrfsMount "/dev/disk/by-uuid/${DISKN_UUID}" "/";
+      "/" = {
+        device = "mindpool/nix";
+        fsType = "zfs";
+      };
     };
-  services.btrfs.autoScrub.enable = true;
   atlas.routerDisk.enable = true;
 }
